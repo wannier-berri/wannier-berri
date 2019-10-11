c5277cfd81c4b188f2168059d5ebcec6e0161d31
stepan-tsirkin <stepan.tsirkin@uzh.ch>
2019-10-11 22:03:44 +0200

    started the gyrotropic module

--- modules/berry.py
+++ modules/berry.py
@@ -44,7 +44,6 @@ def eval_Jo_deg(A,degen):
 #    print (degen)
     return np.array([A[ib1:ib2].sum(axis=0) for ib1,ib2 in degen])
 
-
 def eval_Juo_deg(B,degen):
     return np.array([B[:ib1,ib1:ib2].sum(axis=(0,1)) + B[ib2:,ib1:ib2].sum(axis=(0,1)) for ib1,ib2 in degen])
 
@@ -77,46 +76,7 @@ def get_occ(E_K,Efermi):
     return (E_K< Efermi)
 
 
-def get_degen_bands(E_K,degen_thresh):
-    A=[np.hstack( ([0],np.where(E[1:]-E[:1]>degen_thresh)[0]+1, [E.shape[0]]) ) for E in E_K ]
-    deg= [[(ib1,ib2) for ib1,ib2 in zip(a,a[1:])] for a in A]
-    Eav= [ np.array( [E[b1:b2].mean() for b1,b2 in deg  ]) for E,deg in zip(E_K,deg)]
-    return deg,Eav
 
-
-
-def calcAHC_band(data,Efermi,degen_thresh=None):
-    def function(AHC,Efermi,data,degen,E_K_av,ik):
-        imf=calcImf_K(data,degen,ik )
-        for e,f in zip(E_K_av,imf):
-            AHC[Efermi>e]+=f
-    return calcSmth_band(data,Efermi,(3,) ,function,   degen_thresh=degen_thresh)*fac_ahc/(data.NKFFT_tot*data.cell_volume)
-
-
-def calcMorb_band(data,Efermi,degen_thresh=None):
-    def function(Morb,Efermi,data,degen,E_K_av,ik):
-        imf,img,imh=calcImfgh_K(data,degen,ik )
-        for e,f,g,h in zip(E_K_av,imf,img,imh):
-            sel= Efermi>e
-            Morb[sel]+=(g+h)
-            Morb[sel]+=-2*f[None,:]*Efermi[sel,None]
-    return calcSmth_band(data,Efermi,(3,) ,function,   degen_thresh=degen_thresh)*fac_morb/(data.NKFFT_tot)
-
-
-##  a general procedure to evaluate smth band-by-band in the Fermi sea
-def calcSmth_band(data,Efermi,shape,function, degen_thresh=None):
-    if degen_thresh is None:
-        degen_thresh=-1
-    E_K=data.E_K
-    degen_bands,E_K_av=get_degen_bands(E_K,degen_thresh)
-
-    if not(isinstance(Efermi, Iterable)): 
-        Efermi=np.array([Efermi])
-
-    RES=np.zeros( (len(Efermi),)+tuple(shape) )
-    for ik in range(data.NKFFT_tot) :
-        function(RES,Efermi,data,degen_bands[ik],E_K_av[ik],ik)
-    return RES 
 
 
 def calcImf_K(data,degen_bands,ik):
---
+++ modules/gyrotropic.py
@@ -0,0 +1,49 @@
+import numpy as np
+from collections import Iterable
+
+from berry import fac_ahc, fac_morb, calcImf_K, calcImfgh_K
+
+def calcAHC(data,Efermi,degen_thresh=None):
+    def function(AHC,Efermi,data,degen,E_K_av,ik):
+        imf=calcImf_K(data,degen,ik )
+        for e,f in zip(E_K_av,imf):
+            AHC[Efermi>e]+=f
+    return __calcSmth_band(data,Efermi,(3,) ,function,   degen_thresh=degen_thresh)*fac_ahc/(data.NKFFT_tot*data.cell_volume)
+
+
+def calcMorb(data,Efermi,degen_thresh=None):
+    def function(Morb,Efermi,data,degen,E_K_av,ik):
+        imf,img,imh=calcImfgh_K(data,degen,ik )
+        for e,f,g,h in zip(E_K_av,imf,img,imh):
+            sel= Efermi>e
+            Morb[sel]+=(g+h)
+            Morb[sel]+=-2*f[None,:]*Efermi[sel,None]
+    return __calcSmth_band(data,Efermi,(3,) ,function,   degen_thresh=degen_thresh)*fac_morb/(data.NKFFT_tot)
+
+
+
+##  a general procedure to evaluate smth band-by-band in the Fermi sea
+def __calcSmth_band(data,Efermi,shape,function, degen_thresh=None):
+    if degen_thresh is None:
+        degen_thresh=-1
+    E_K=data.E_K
+    degen_bands,E_K_av=__get_degen_bands(E_K,degen_thresh)
+
+    if not(isinstance(Efermi, Iterable)): 
+        Efermi=np.array([Efermi])
+
+    RES=np.zeros( (len(Efermi),)+tuple(shape) )
+    for ik in range(data.NKFFT_tot) :
+        function(RES,Efermi,data,degen_bands[ik],E_K_av[ik],ik)
+    return RES 
+
+
+def __get_degen_bands(E_K,degen_thresh):
+    A=[np.hstack( ([0],np.where(E[1:]-E[:1]>degen_thresh)[0]+1, [E.shape[0]]) ) for E in E_K ]
+    deg= [[(ib1,ib2) for ib1,ib2 in zip(a,a[1:])] for a in A]
+    Eav= [ np.array( [E[b1:b2].mean() for b1,b2 in deg  ]) for E,deg in zip(E_K,deg)]
+    return deg,Eav
+
+
+def __average_degen(X,degen):
+    np.array( [X[b1:b2].mean(axis=0) for b1,b2 in degen  ]) 
