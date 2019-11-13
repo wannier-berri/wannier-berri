87c6e37c5a50c5c1cbf2c674a225f43463fb7e36
Stepan Tsirkin <stepan.tsirkin@uzh.ch>
2019-11-13 14:53:19 +0100

    Update __init__.py

--- wannier19/__init__.py
+++ wannier19/__init__.py
@@ -1,3 +1,3 @@
-from .__wannier19_main import integrate,tabulate,Data,symmetry,integrate_options,tabulate_options,__version__
+from .__wannier19_main import integrate,tabulate,Data,symmetry,integrate_options,tabulate_options
 from .__version import __version__
 
