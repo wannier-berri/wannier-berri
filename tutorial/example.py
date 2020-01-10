4470ce0f6e676a3c3e4ff83b46dab95b685e6da7
stepan-tsirkin <stepan.tsirkin@ehu.eus>
2020-01-11 00:32:01 +0100

    renamed to WannierBerri

--- tutorial/example.py
+++ tutorial/example.py
@@ -2,40 +2,41 @@
 
 
 ## these linesline if you want to use the git version of the code, instead of the one installed by pip
-local_code=False# True
+local_code=True #False
 num_proc=2
 
 import os
 
 if local_code:
-   if 'wannier19' not in os.listdir() :
-       os.symlink("../wannier19","wannier19")
+   if 'wannierberri' not in os.listdir() :
+       os.symlink("../wannierberri","wannierberri")
 else:
-   if 'wannier19' in os.listdir() :
-       os.remove('wannier19')
+   if 'wannierberri' in os.listdir() :
+       os.remove('wannierberri')
 
 if 'Fe_tb.dat' not in os.listdir():
     os.system('tar -xvf ../data/Fe_tb.dat.tar.gz') 
 
 
-import wannier19 as w19
+import wannierberri as wberri
+
 import numpy as np
 
 
-SYM=w19.symmetry
+SYM=wberri.symmetry
 
 seedname="Fe"
 NK=96
 
 name=seedname
 Efermi=np.linspace(12.,13.,1001)
-system=w19.System(tb_file='Fe_tb.dat',getAA=True)
+system=wberri.System(tb_file='Fe_tb.dat',getAA=True)
 
 generators=[SYM.Inversion,SYM.C4z,SYM.TimeReversal*SYM.C2x]
 
 
 if False:
-   w19.tabulate(system,
+   wberri.tabulate(system,
              NK=NK,
              quantities=["V","berry"],
              symmetry_gen=generators,
@@ -45,7 +46,7 @@ if False:
              restart=False)
 
 
-w19.integrate(system,
+wberri.integrate(system,
             NK=NK,
             Efermi=Efermi, 
             smearEf=10,
