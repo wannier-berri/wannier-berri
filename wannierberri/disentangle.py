174fce90057fe34ce4bcb8945f43398a1874d06a
Stepan Tsirkin <stepan.tsirkin@uzh.ch>
2020-06-11 16:48:45 +0200

    in principle wrks, but strange bug arises

--- wannierberri/disentangle.py
+++ wannierberri/disentangle.py
@@ -6,11 +6,8 @@ class WannierModel():
 # todo :  write the files
 # todo :  rotatre uHu and spn
 # todo : create a model from this
-# todo : calculate Omega_I
 # todo : symmetry
 
-#todo :  define mp_grid from kpt_latt
-#todo : read real_lattice and kpt_latt form .win
     def __init__(self,seedname="wannier90"):
         win=WIN(seedname)
 #        win.print_clean()
@@ -26,22 +23,28 @@ class WannierModel():
         mmn=MMN(seedname)
         amn=AMN(seedname)
         assert eig.NK==amn.NK==mmn.NK
+        assert eig.NB>=amn.NB
+        assert eig.NB>=mmn.NB
         self.NK=eig.NK
         self.NW=amn.NW
         self.recip_lattice=2*np.pi*np.linalg.inv(self.real_lattice)
         mmn.set_bk(self.mp_grid,self.kpt_latt,self.recip_lattice)
         self.bk=mmn.bk
-        self.wk=mmn.wk
+        self.wb=mmn.wk
         self.neighbours=mmn.neighbours
         self.Mmn=mmn.data
         self.Amn=amn.data
         self.Eig=eig.data
+        self.win_index=[np.arange(eig.NB)]*self.NK
+
+
+
 
     # TODO : allow k-dependent window (can it be useful?)
     def apply_outer_window(self,
                      win_min=-np.Inf,
                      win_max= np.Inf ):
-        self.win_index=[np.where( ( E<=win_max)*(E>=win_min) )[0] for E in self.Eig]
+        self.win_index=[ind[np.where( ( E<=win_max)*(E>=win_min) )[0]] for ind,E in zip (self.win_index,self.Eig)]
         self.Eig=[E[ind] for E, ind in zip(self.Eig,self.win_index)]
         self.Mmn=[[self.Mmn[ik][ib][self.win_index[ik],:][:,self.win_index[ikb]] for ib,ikb in enumerate(self.neighbours[ik])] for ik in range(self.NK)]
         self.Amn=[self.Amn[ik][self.win_index[ik],:] for ik in range(self.NK)]
@@ -60,32 +63,35 @@ class WannierModel():
         self.free= [ np.logical_not(frozen) for frozen in self.frozen]
         self.nBfree=[ np.sum(free) for free in self.free ]
         self.nWfree=[ self.NW-np.sum(frozen) for frozen in self.frozen]
+        print ("Bfree:",self.nBfree)
+        print ("Wfree:",self.nWfree)
         # initial guess : eq 27 of SMV2001
         U_opt_free=self.get_max_eig(  [ A[free,:].dot(A[free,:].T.conj()) 
                         for A,free in zip(self.Amn,self.free)]  ,self.nWfree) # nBfee x nWfree marrices
 
+        Mmn_FF=self.Mmn_Free_Frozen(self.Mmn,self.free,self.frozen,self.neighbours,self.wb,self.NW)
         def calc_Z(Mmn_loc,U=None):
             if U is None: 
                Mmn_loc_opt=Mmn_loc
             else:
-               Mmn_loc_opt=[[Mmn[ib].dot(U[ikb]) for ib,ikb in enumerate(neigh)] for Mmn,neigh in zip(Mmn_free,self.neighbours)]
-            return [sum(wb*mmn.dot(mmn.T.conj()) for wb,mmn in zip(self.wb,Mmn)) for Mmn in Mmn_loc_opt ]
+               Mmn_loc_opt=[[Mmn[ib].dot(U[ikb]) for ib,ikb in enumerate(neigh)] for Mmn,neigh in zip(Mmn_FF('free','free'),self.neighbours)]
+            return [sum(wb*mmn.dot(mmn.T.conj()) for wb,mmn in zip(wbk,Mmn)) for wbk,Mmn in zip(self.wb,Mmn_loc_opt) ]
 
-        Mmn_free_frozen=[[self.Mmn[ik][ib][self.free[ik],:][:,self.frozen[ikb]] 
-                             for ib,ikb in enumerate(self.neighbours[ik])] for ik in range(self.NK)] 
-        Z_frozen=calc_Z(Mmn_free_frozen)
-        del Mmn_free_frozen  # we do not need it anymore, I think ))
-        Mmn_free=[[self.Mmn[ik][ib][self.free[ik],:][:,self.free[ikb]] for ib,ikb in enumerate(self.neighbours[ik])] for ik in range(self.NK)] 
+        Z_frozen=calc_Z(Mmn_FF('free','frozen'))
+#        del Mmn_free_frozen  # we do not need it anymore, I think ))
+        
 
-        print ( '+---------------------------------------------------------------------+<-- DIS\n'+
-                '|  Iter     Omega_I(i-1)      Omega_I(i)      Delta (frac.)    Time   |<-- DIS\n'+
-                '+---------------------------------------------------------------------+<-- DIS'  )
+#        print ( '+---------------------------------------------------------------------+<-- DIS\n'+
+#                '|  Iter     Omega_I(i-1)      Omega_I(i)      Delta (frac.)    Time   |<-- DIS\n'+
+#                '+---------------------------------------------------------------------+<-- DIS'  )
 
         for i_iter in range(num_iter):
-            Z=[(z+zfr)  for z,zfr in zip(calc_Z(Mmn_free,U_opt_free),Z_frozen)]
+            Z=[(z+zfr)  for z,zfr in zip(calc_Z(Mmn_FF('free','free'),U_opt_free),Z_frozen)]
             if i_iter>0 and mix_ratio<1:
                 Z=[ (mix_ratio*z + (1-mix_ratio)*zo) for z,zo in zip(Z,Z_old) ]
             U_opt_free=self.get_max_eig(Z,self.nWfree)
+            Omega_I=Mmn_FF.Omega_I(U_opt_free)
+            print ("iteration {:4d}".format(i_iter)+" Omega_I= "+"  ".join("{:15.10f}".format(x) for x in Omega_I)+" tot =","{:15.10f}".format(sum(Omega_I)))
             Z_old=deepcopy(Z)
 
         U_opt_full=[]
@@ -102,10 +108,11 @@ class WannierModel():
        # now rotating to the optimized space
         for ik in range(self.NK):
             U=U_opt_full[ik]
+            Ud=U.T.conj()
             self.Eig[ik]=(Ud.dot(np.diag(self.Eig[ik])).dot(U)).real
             self.Amn[ik]=Ud.dot(self.Amn[ik])
-            for ib,ikb in enumerate (self.neighbours[ik]):
-                self.Mmn[ik]=[Ud.dot(M).dot(U_opt_full[ibk]) for M,ibk in zip (self.Mmn[ik],self.neighbours[ik])]
+#            for ib,ikb in enumerate (self.neighbours[ik]):
+            self.Mmn[ik]=[Ud.dot(M).dot(U_opt_full[ibk]) for M,ibk in zip (self.Mmn[ik],self.neighbours[ik])]
            
     def rotate(self,mat,ik1,ik2):
         # data should be of form NBxNBx ...   - any form later
@@ -123,11 +130,52 @@ class WannierModel():
     def get_max_eig(self,matrix,nvec):
         """ return the nvec column-eigenvectors of matrix with maximal eigenvalues. 
         Both matrix and nvec are lists by k-points,s with arbitrary size of matrices"""
-        print ("getting maximal vectors of: \n {}".format(matrix))
+#        print ("getting maximal vectors of: \n {}".format(matrix))
         assert len(matrix)==len(nvec)==self.NK
         assert np.all([m.shape[0]==m.shape[1] for m in matrix])
-        assert np.all([m.shape[0]<nv for m,nv in zip(matrix,nvec)])
+        assert np.all([m.shape[0]>=nv for m,nv in zip(matrix,nvec)]), "nvec={}, m.shape={}".format(nvec,[m.shape for m in matrix])
         EV=[np.linalg.eigh(M) for M in matrix]
         return [ ev[1][:,np.argsort(ev[0])[-nv:]] for ev,nv  in zip(EV,nvec) ] # nBfee x nWfree marrices
 
+    class Mmn_Free_Frozen():
+        def __init__(self,Mmn,free,frozen,neighbours,wb,NW):
+           self.NK=len(Mmn)
+           self.wb=wb
+           self.neighbours=neighbours
+           self.data={}
+           self.spaces={'free':free,'frozen':frozen}
+           for s1,sp1 in self.spaces.items():
+               for s2,sp2 in self.spaces.items():
+                   self.data[(s1,s2)]=[[Mmn[ik][ib][sp1[ik],:][:,sp2[ikb]] 
+                             for ib,ikb in enumerate(neigh)] for ik,neigh in enumerate(self.neighbours)]
+#           print ("wb=",self.wb)
+#           print ( len(mmn) for mmn in self('frozen','frozen'))
+           self.Omega_I_0=NW*self.wb[0].sum()
+           self.Omega_I_frozen=-sum( sum( wb*np.sum(abs(mmn[ib])**2) for ib,wb in enumerate(WB)) for WB,mmn in zip(self.wb,self('frozen','frozen')))/self.NK
+
+        def __call__(self,space1,space2):
+            assert space1 in self.spaces
+            assert space2 in self.spaces
+            return self.data[(space1,space2)]
+
+        def Omega_I_free_free(self,U_opt_free):
+            U=U_opt_free
+            Mmn=self('free','free')
+            return -sum( self.wb[ik][ib]*np.sum(abs(   U[ik].T.conj().dot(Mmn[ib]).dot(U[ikb])  )**2) 
+                        for ik,Mmn in enumerate(Mmn) for ib,ikb in enumerate(self.neighbours[ik])  )/self.NK
+
+        def Omega_I_free_frozen(self,U_opt_free):
+            U=U_opt_free
+            Mmn=self('free','frozen')
+            return -sum( self.wb[ik][ib]*np.sum(abs(   U[ik].T.conj().dot(Mmn[ib])  )**2) 
+                        for ik,Mmn in enumerate(Mmn) for ib,ikb in enumerate(self.neighbours[ik])  )/self.NK*2
+
+#        def Omega_I_frozen_free(self,U_opt_free):
+#            U=U_opt_free
+#            Mmn=self('frozen','free')
+#            return -sum( self.wb[ik][ib]*np.sum(abs(  Mmn[ib].dot(U[ikb])  )**2) 
+#                        for ik,Mmn in enumerate(Mmn) for ib,ikb in enumerate(self.neighbours[ik])  )/self.NK
+
+        def Omega_I(self,U_opt_free):
+            return self.Omega_I_0,self.Omega_I_frozen,self.Omega_I_free_frozen(U_opt_free),self.Omega_I_free_free(U_opt_free)
 
