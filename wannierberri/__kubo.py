58925759c9147e171ba713b83d98f05ce7f6a6d8
Jae-Mo Lihm <jaemolim96@gmail.com>
2020-08-14 21:14:30 +0900

    Remove trailing whitespaces

--- wannierberri/__kubo.py
+++ wannierberri/__kubo.py
@@ -19,7 +19,7 @@ import numpy as np
 from scipy import constants as constants
 from collections import Iterable
 import functools
-from termcolor import cprint 
+from termcolor import cprint
 
 from . import __result as result
 
@@ -34,7 +34,7 @@ def Lorentzian(x, width):
 def Gaussian(x, width):
     arg = np.minimum(200.0, (x/width)**2)
     return 1.0/(np.sqrt(pi) * width) * np.exp(-1*arg)
-    
+
 # Fermi-Dirac distribution
 def FermiDirac(E, mu, kBT):
     if kBT == 0:
@@ -48,7 +48,7 @@ def opt_conductivity(data, omega=0, mu=0, kBT=0, smr_fixed_width=0.1, smr_type='
                 adpt_smr_fac=np.sqrt(2), adpt_smr_max=0.1, adpt_smr_min=1e-15):
     '''
     Calculates the optical conductivity according to the Kubo-Greenwood formula.
-    
+
     Arguments:
         data            instance of __data_dk.Data_dk representing a single point in the BZ
         omega           value or list of frequencies in units of eV/hbar
@@ -60,11 +60,11 @@ def opt_conductivity(data, omega=0, mu=0, kBT=0, smr_fixed_width=0.1, smr_type='
         adpt_smr_fac    prefactor for the adaptive smearing parameter
         adpt_smr_max    maximal value of the adaptive smearing parameter
         adpt_smr_min    minimal value of the adaptive smearing parameter
-        
+
     Returns:    a list of (complex) optical conductivity 3 x 3 tensors (one for each frequency value).
                 The result is given in S/cm.
     '''
-    
+
     # data gives results in terms of
     # ik = index enumerating the k points in Data_dk object
     # m,n = indices enumerating the eigenstates/eigenvalues of H(k)
@@ -72,35 +72,35 @@ def opt_conductivity(data, omega=0, mu=0, kBT=0, smr_fixed_width=0.1, smr_type='
     # additionally the result will include
     # iw = index enumerating the frequency values
     # ri = index for real and imaginary parts (0 -> real, 1 -> imaginary)
-    
+
     # TODO: optimize for T = 0? take only necessary elements
-    
+
     # prefactor for correct units of the result (S/cm)
     pre_fac = e**2/(100.0 * hbar * data.NKFFT_tot * data.cell_volume * constants.angstrom)
-    
+
     # frequency
     if not isinstance(omega, Iterable):
         omega = np.array([omega])
-        
+
     sigma_H = np.zeros((omega.shape[0], 3, 3), dtype=np.dtype('complex128'))
     sigma_AH = np.zeros((omega.shape[0], 3, 3), dtype=np.dtype('complex128'))
-        
+
     # iterate over ik
     for ik in range(data.NKFFT_tot):
         # energy
         E = data.E_K[ik] # energies [n] in eV
         dE = E[np.newaxis,:] - E[:, np.newaxis] # E_m(k) - E_n(k) [n, m]
-        
+
          # occupation
         fE = FermiDirac(E, mu, kBT) # f(E_m(k)) - f(E_n(k)) [n]
         dfE = fE[np.newaxis,:] - fE[:, np.newaxis] # [n, m]
-        
+
         # generalized Berry connection matrix
         A = data.A_H[ik] # [n, m, a] in angstrom
-       
+
         # E - omega
         delta_arg = dE - omega[:, np.newaxis, np.newaxis] # argument of delta function [iw, n, m]
-        
+
         # smearing
         if adpt_smr: # [iw, n, m]
             cprint("Adaptive smearing is an experimental feature and has not been extensively tested.", 'orange')
@@ -121,28 +121,28 @@ def opt_conductivity(data, omega=0, mu=0, kBT=0, smr_fixed_width=0.1, smr_type='
         else:
             cprint("Invalid smearing type. Fallback to Lorentzian", 'orange')
             delta = Lorentzian(delta_arg, eta)
-        
+
         sigma_H += -1 * pi * pre_fac * np.einsum('nm,nm,nma,mnb,wnm->wab', dfE, dE, A, A, delta) # [iw, a, b]
-        
+
         # free memory
         del delta
-        
-        
+
+
         # anti-Hermitian part of the conductivity tensor
         re_efrac = delta_arg/(delta_arg**2 + eta**2) # real part of energy fraction [iw, n, m]
         sigma_AH += 1j * pre_fac * np.einsum('nm,nm,wnm,nma,mnb->wab', dfE, dE, re_efrac, A, A) # [iw, a, b]
-        
+
         # free memory
         del re_efrac
         del delta_arg
         del dfE
         del dE
-        
+
     # TODO: optimize by just storing independent components or leave it like that?
     # 3x3 tensors [iw, a, b]
     sigma_sym = np.real(sigma_H) + 1j * np.imag(sigma_AH) # symmetric (TR-even, I-even)
     sigma_asym = np.real(sigma_AH) + 1j * np.imag(sigma_H) # ansymmetric (TR-odd, I-even)
-    
+
     # return result dictionary
     return result.EnergyResultDict({
         'sym':  result.EnergyResult(omega, sigma_sym, TRodd=False, Iodd=False, rank=2),
