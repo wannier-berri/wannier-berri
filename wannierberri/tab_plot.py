b9cae037830652961307a6840f2b0c99ed49c9ee
Xiaoxiong Liu <Xiaoxiong.Liu@uzh.ch>
2020-11-04 17:36:01 +0100

    fix bugs

--- wannierberri/tab_plot.py
+++ wannierberri/tab_plot.py
@@ -14,37 +14,62 @@
 # Xiaoxiong Liu, University of Zurich                        #
 # Email: xxliu@physik.uzh.ch                                 #
 #------------------------------------------------------------#
-'''This utility calculates the matrices .uHu and/or .uIu from the .mmn matrices, and also reduces the number of bands in .amn, .mmn, .eig  and .spn files
+'''This is a script to plot the band with line path (High symmetry line) or plane cut using tabulate calculation result from Wannierberri. 
+
+	NOTE: 1. Please chose the line kpaths which are in the 1st BZ.
+	      2. The plane cut figure shows a plane cut of 2x2x2 reciprocal lattice. 
+	      3. All k coordinate values in list like parameters should be integer. (means which k point).
+		 eg. If k-grid is 12x12x12. kpoint (1/3,2/3,0.5) should be (4,8,6)
+	      4. In order to ensure that more high symmetry points have k-grid points, you would better set NK as multiples of 12 when doing tabulate calculation. 
 
         Usage example:
-                python3 -m wanierberri.mmn2uHu seedname NBout=10 NBsum=100,200  targets=mmn,uHu formatted=uHu
+                line:  python3 -m wannierberri.tab_plot wannier90_spin-z type=Line quantity=True kpath=0,0,0,0,0,40 namelist=G,Z
+                plane: python3 -m wannierberri.tab_plot wannier90_spin-z type=Plane quantity=True Efermi=-0.5 vec1=1,0,0 vec2=0,1,0
                 
         Options
             -h 
                 | print the help message
-            IBstart
-                |  the first band in the output file (counting starts from 1). 
-                |  default: 1
-            IBstartSum
-                |  the first band in the sum         (counting starts from 1). 
-                |  default: 1
-            NBout 
-                |  the number of bands in the output files. 
-                |  Default : all bands
-            NBsum 
-                |  the number of bands in the summation. (one may specify several numbers, usefull to test convergence with the number of bands). 
-                |  Default:all bands
-            input 
-                |  path to the input files. 
-                |  Default: ./  
-            output 
-                |  path to the output files 
-            targets 
-                |  files to write : ``amn``, ``mmn``, ``spn``, ``uHu``, ``uIu``, ``eig`` 
-                |  default: ``amn``,``mmn``,``eig``,``uHu``
-            formatted 
-                |  files to write as formatted  ``uHu``, ``uIu``, ``spn``, ``spn_in``, ``spn_out``, ``all`` 
-                |  default: none
+            type (String)
+                |  Type of plot. 
+		|  	Line: line cut
+		|  	Plane: plane cut 
+                |  Default: None
+            quantity (Boolean)
+                |  Plot quantity or not.
+		|  	False: Only plot energy of band.
+		|	True: Not only energy of band but also quantity(plot as color dot)  
+                |  Default: False
+            formatted (Boolean)
+                |  Format of input file.
+		|	False: binary
+		|	True: txt 
+		|  It should be same as parameter 'formatted' in tabulate.  
+                |  Default: False
+            o_point (list like)
+		|  k coordinate of origin.(type=Plane)
+		|  Two vectors and one origin can define a plane. 
+                |  Default: 0,0,0
+            vec1 (list like) 
+                |  k coordinate of one of the two vectors. (type=Plane)
+		|  Only direction of vector work. (0,0,1) == (0,0,6)
+		|  And it is the horizontal axis of plot.  
+                |  Default: 0,0,1  
+            vec2 (list like) 
+                |  k coordinate of one of the two vectors. (type=Plane)
+		|  Only direction of vector work. (0,1,0) == (0,6,0)
+                |  Default: 0,1,0  
+            kpath (list like)
+                |  Starting points and ending points of k-path. (type=Plane)
+		|  6 elements are one group, the first three elements are k coordinate of starting point and the back three elements are k coordinate of ending point. It should have multiples of 6 elements.
+		|  Default: 0,0,0,0,0,40 
+            Efermi (float) 
+                |  Fermi level when (type=Line)
+		|  Plotting energy (when type=Plane) 
+                |  default: 0.0
+            E_min (float) 
+	    E_max (float)
+                |  Energy window of plot. (type=Line) 
+                |  default: E_min=-2 Emax=2
 
 '''
 
@@ -95,7 +120,7 @@ def main():
 		if arg[0]=='vec2': vec2=np.array([x for x in arg[1].split(',')],dtype=int)
 		if arg[0]=='kpath': 
 			tmp=np.array([x for x in arg[1].split(',')],dtype=int)
-			kpath=tmp.reshape(len(arg[1].split)//3,3)
+			kpath=tmp.reshape(len(tmp)//3,3)
 		if arg[0]=='Efermi': Efermi=float(arg[1])
 		if arg[0]=='E_min': E_min=float(arg[1])
 		if arg[0]=='E_max': E_min=float(arg[1])
@@ -138,6 +163,78 @@ def main():
 	print('k-vectors')
 	print(LATTICE)
 
+	def module(vec):
+		return (vec[0]**2+vec[1]**2+vec[2]**2)**0.5
+
+	def kline(nkp):
+		n_l = (K-(kpath[2*nkp])[:,None,None,None])[:,:,:,:]
+		l_m = (kpath[2*nkp]-kpath[2*nkp+1])[:,None,None,None]
+		m_l = (kpath[2*nkp+1]-kpath[2*nkp])[:,None,None,None]
+		cross = abs(n_l[1,:,:,:] * l_m[2] - n_l[2,:,:,:] * l_m[1]) + abs(n_l[0,:,:,:] * l_m[2] - n_l[2,:,:,:] * l_m[0]) +  abs(n_l[0,:,:,:] * l_m[1] - n_l[1,:,:,:] * l_m[0])
+		dot = n_l[0,:,:,:] * m_l[0] + n_l[1,:,:,:] * m_l[1] + n_l[2,:,:,:] * m_l[2]
+		select1 = cross == 0
+		select2 = dot >= 0
+		select3 = dot <= np.dot(kpath[2*nkp+1] - kpath[2*nkp],kpath[2*nkp+1]-kpath[2*nkp])
+		select = select1*select2*select3
+		positions = K[:,select]
+		print('=_____________________________=')
+		print('kpath = ',nkp+1)
+		print('num of kpoint = ',len(positions[0]) )
+		print(positions.transpose(1,0))
+		eig = EIGMAT[:,select]
+		kpatheig = np.zeros((NB,len(eig[0]),3))
+		if quantity:	
+			kpatheig = np.zeros((NB,len(eig[0]),3))
+			quan = QMAT[:,select]
+			kpatheig[:,:,1] = quan
+		else:
+			kpatheig = np.zeros((NB,len(eig[0]),2))
+		kpatheig[:,:,0] = eig
+		for i in range(len(positions[0])):
+			dis = KPOP[nkp] + module(np.dot(positions[:,i]-kpath[2*nkp],LATTICE)) 
+			kpatheig[:,i,-1] = dis
+		if quantity:
+			return kpatheig.reshape(NB*len(eig[0]),3)
+		else:
+			return kpatheig.reshape(NB*len(eig[0]),2)
+
+	def kplane():
+		cross_p = np.array([vec1[1] * vec2[2] - vec1[2] * vec2[1], vec1[0] * vec2[2] - vec1[2] * vec2[0] ,vec1[0] * vec2[1] - vec1[1] * vec2[0]])[:,None,None,None]
+		Kvec = (K-o_point[:,None,None,None])[:,:,:,:]
+		dot_p = Kvec[0,:,:,:]*cross_p[0] + Kvec[1,:,:,:]*cross_p[1] + Kvec[2,:,:,:]*cross_p[2] 
+		select_p = dot_p==0
+		positions_p = K[:,select_p]
+		eig_p = EIGMAT[:,select_p]
+		kc = np.dot(positions_p.transpose(1,0)/NK,LATTICE)
+		kcv1 = np.dot(vec1,LATTICE)
+		kcv2 = np.dot(vec2,LATTICE)
+		V = np.dot(np.cross(LATTICE[0],LATTICE[1]),LATTICE[2])
+		x=kcv1
+		z=np.cross(kcv1,kcv2)
+		z=z*V/np.dot(z,z)
+		y=np.cross(x,z)
+		y=y*V/np.dot(y,y)
+		xlen=module(kcv1)
+		angle = np.arccos(np.dot(kcv1,kcv2)/(module(kcv1)*module(kcv2)))
+		ylen=module(kcv1)*np.sin(angle)
+		kx = np.dot(kc,x)
+		kx = kx*xlen/max(kx)
+		ky = np.dot(kc,y)
+		ky = ky*ylen/max(ky)
+		if quantity:
+			kplaneeig = np.zeros((NB,len(eig_p[0]),4))
+			quan_p = QMAT[:,select_p]
+			kplaneeig[:,:,1] = quan_p
+		else:
+			kplaneeig = np.zeros((NB,len(eig_p[0]),3))
+		kplaneeig[:,:,0] = eig_p
+		kplaneeig[:,:,-2] = kx
+		kplaneeig[:,:,-1] = ky
+	
+		if quantity:
+			return kplaneeig.reshape(NB*len(eig_p[0]),4)
+		else:
+			return kplaneeig.reshape(NB*len(eig_p[0]),3)
 	###################### main #############################
 	if Line:
 		NKP = len(kpath)//2  #number of kpath
@@ -184,81 +281,8 @@ def main():
 		plt.savefig('Plane_eig.png')
 #		plt.show()		
 
-
-
-def module(vec):
-	return (vec[0]**2+vec[1]**2+vec[2]**2)**0.5
-
-def kline(nkp):
-	n_l = (K-(kpath[2*nkp])[:,None,None,None])[:,:,:,:]
-	l_m = (kpath[2*nkp]-kpath[2*nkp+1])[:,None,None,None]
-	m_l = (kpath[2*nkp+1]-kpath[2*nkp])[:,None,None,None]
-	cross = abs(n_l[1,:,:,:] * l_m[2] - n_l[2,:,:,:] * l_m[1]) + abs(n_l[0,:,:,:] * l_m[2] - n_l[2,:,:,:] * l_m[0]) +  abs(n_l[0,:,:,:] * l_m[1] - n_l[1,:,:,:] * l_m[0])
-	dot = n_l[0,:,:,:] * m_l[0] + n_l[1,:,:,:] * m_l[1] + n_l[2,:,:,:] * m_l[2]
-	select1 = cross == 0
-	select2 = dot >= 0
-	select3 = dot <= np.dot(kpath[2*nkp+1] - kpath[2*nkp],kpath[2*nkp+1]-kpath[2*nkp])
-	select = select1*select2*select3
-	positions = K[:,select]
-	print('=_____________________________=')
-	print('kpath = ',nkp+1)
-	print('num of kpoint = ',len(positions[0]) )
-	print(positions.transpose(1,0))
-	eig = EIGMAT[:,select]
-	kpatheig = np.zeros((NB,len(eig[0]),3))
-	if quantity:	
-		kpatheig = np.zeros((NB,len(eig[0]),3))
-		quan = QMAT[:,select]
-		kpatheig[:,:,1] = quan
-	else:
-		kpatheig = np.zeros((NB,len(eig[0]),2))
-	kpatheig[:,:,0] = eig
-	for i in range(len(positions[0])):
-		dis = KPOP[nkp] + module(np.dot(positions[:,i]-kpath[2*nkp],LATTICE)) 
-		kpatheig[:,i,-1] = dis
-	if quantity:
-		return kpatheig.reshape(NB*len(eig[0]),3)
-	else:
-		return kpatheig.reshape(NB*len(eig[0]),2)
-
-def kplane():
-	cross_p = np.array([vec1[1] * vec2[2] - vec1[2] * vec2[1], vec1[0] * vec2[2] - vec1[2] * vec2[0] ,vec1[0] * vec2[1] - vec1[1] * vec2[0]])[:,None,None,None]
-	Kvec = (K-o_point[:,None,None,None])[:,:,:,:]
-	dot_p = Kvec[0,:,:,:]*cross_p[0] + Kvec[1,:,:,:]*cross_p[1] + Kvec[2,:,:,:]*cross_p[2] 
-	select_p = dot_p==0
-	positions_p = K[:,select_p]
-	eig_p = EIGMAT[:,select_p]
-	kc = np.dot(positions_p.transpose(1,0)/NK,LATTICE)
-	kcv1 = np.dot(vec1,LATTICE)
-	kcv2 = np.dot(vec2,LATTICE)
-	V = np.dot(np.cross(LATTICE[0],LATTICE[1]),LATTICE[2])
-	x=kcv1
-	z=np.cross(kcv1,kcv2)
-	z=z*V/np.dot(z,z)
-	y=np.cross(x,z)
-	y=y*V/np.dot(y,y)
-	xlen=module(kcv1)
-	angle = np.arccos(np.dot(kcv1,kcv2)/(module(kcv1)*module(kcv2)))
-	ylen=module(kcv1)*np.sin(angle)
-	kx = np.dot(kc,x)
-	kx = kx*xlen/max(kx)
-	ky = np.dot(kc,y)
-	ky = ky*ylen/max(ky)
-	if quantity:
-		kplaneeig = np.zeros((NB,len(eig_p[0]),4))
-		quan_p = QMAT[:,select_p]
-		kplaneeig[:,:,1] = quan_p
-	else:
-		kplaneeig = np.zeros((NB,len(eig_p[0]),3))
-	kplaneeig[:,:,0] = eig_p
-	kplaneeig[:,:,-2] = kx
-	kplaneeig[:,:,-1] = ky
-
-	if quantity:
-		return kplaneeig.reshape(NB*len(eig_p[0]),4)
-	else:
-		return kplaneeig.reshape(NB*len(eig_p[0]),3)
-
-		 
+	
+if __name__ == "__main__":
+	main()		 
 
 
