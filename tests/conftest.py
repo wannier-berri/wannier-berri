4a7ef13dbaff3a469e462343803be010e9b15a1e
Jae-Mo Lihm <jaemolim96@gmail.com>
2021-03-22 18:28:53 +0900

    Add automatic test using pytest.

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+"""pytest configuration file for WannierBerri tests."""
+
+import os
+
+import numpy
+import pytest
+
+@pytest.fixture(scope="session")
+def rootdir():
+    return os.path.dirname(os.path.abspath(__file__))
+
