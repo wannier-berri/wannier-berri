b4f3644ed1e5a46972bd7aa44b5e4fc864fcd318
Stepan Tsirkin <stepan@physik.uzh.ch>
2021-05-14 10:06:37 +0200

    removed unneeded fixture in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,3 @@ import pytest
 @pytest.fixture(scope="session")
 def rootdir():
     return os.path.dirname(os.path.abspath(__file__))
-
-
-@pytest.fixture(scope="session")
-def Efermi_Fe():
-    return os.path.dirname(os.path.abspath(__file__))
-
