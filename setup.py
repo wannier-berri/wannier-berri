f16f23a0e14a06264323587d1e74dbeb063db06c
Liu-Xiaoxiong <xxliu@physik.uzh.ch>
2021-05-15 00:49:38 +0200

    do we need install tbm ptb

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setuptools.setup(
      description="Advanced tool for Wannier interpolation",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     install_requires=['pythtb','tbmodels','numpy', 'scipy >= 1.0', 'lazy_property','colorama','termcolor','pyfiglet','termcolor','pyFFTW>=0.12.0', 'packaging>=20.8','matplotlib','fortio>=0.4'],
+     #install_requires=['pythtb','tbmodels','numpy', 'scipy >= 1.0', 'lazy_property','colorama','termcolor','pyfiglet','termcolor','pyFFTW>=0.12.0', 'packaging>=20.8','matplotlib','fortio>=0.4'],
+     install_requires=['numpy', 'scipy >= 1.0', 'lazy_property','colorama','termcolor','pyfiglet','termcolor','pyFFTW>=0.12.0', 'packaging>=20.8','matplotlib','fortio>=0.4'],
      url="https://github.com/stepan-tsirkin/wannier-berri",
      packages=setuptools.find_packages(),
      classifiers=[
