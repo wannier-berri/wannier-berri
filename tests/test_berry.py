513a33d4675cc609fc5e9f3fec4e7008a32c12c9
Liu-Xiaoxiong <xxliu@physik.uzh.ch>
2021-05-19 19:52:06 +0200

    test system

--- tests/test_berry.py
+++ tests/test_berry.py
@@ -6,10 +6,10 @@ from pytest import approx
 
 import wannierberri as wberri
 
-from create_system import create_files_Fe_W90,  create_files_GaAs_W90,  system_Fe_W90, system_Fe_W90_wcc,system_GaAs_W90 ,system_GaAs_W90_wcc
+from create_system import *
 from compare_result import compare_energyresult
 
-def check_integrate(system,quantities,fout_name,Efermi,comparer,numproc=0,grid_param={'NK':[6,6,6],'NKFFT':[3,3,3]},additional_parameters={},adpt_num_iter=1,suffix="",precision=1e-10,extra_precision={}):
+def check_integrate(system,quantities,fout_name,Efermi,comparer,numproc=0,grid_param={'NK':[6,6,6],'NKFFT':[3,3,3]},additional_parameters={},adpt_num_iter=1,suffix="",extra_precision={}):
     grid = wberri.Grid(system, **grid_param)
     result = wberri.integrate(system,
             grid = grid,
@@ -30,7 +30,7 @@ def check_integrate(system,quantities,fout_name,Efermi,comparer,numproc=0,grid_p
         data=result.results.get(quant).data
         assert data.shape[0] == len(Efermi)
         assert np.all( np.array(data.shape[1:]) == 3)
-        prec=extra_precision[quant] if quant in extra_precision else precision
+        prec=extra_precision[quant] if quant in extra_precision else None
         comparer(fout_name, quant+suffix,  adpt_num_iter , suffix_ref=compare_quant(quant) ,precision=prec )
 
 
@@ -43,11 +43,17 @@ def Efermi_Fe():
 def Efermi_GaAs():
     return np.linspace(7,9,11)
 
+@pytest.fixture(scope="module")
+def Efermi_Haldane():
+    return np.linspace(-3,3,11)
 
 @pytest.fixture(scope="module")
 def quantities_Fe():
     return  ['ahc','ahc_ocean','dos','cumdos'  ,'conductivity_ohmic','conductivity_ohmic_fsurf']
 
+@pytest.fixture(scope="module")
+def quantities_Haldane():
+    return  ['ahc','ahc_ocean','dos' ,'conductivity_ohmic']
 
 @pytest.fixture(scope="module")
 def quantities_GaAs():
@@ -62,7 +68,6 @@ def compare_quant(quant):
         return quant
 
 
-
 def test_Fe(system_Fe_W90, compare_energyresult,quantities_Fe,Efermi_Fe):
     """Test anomalous Hall conductivity , ohmic conductivity, dos, cumdos"""
     check_integrate(system_Fe_W90 , quantities_Fe , fout_name="berry_Fe_W90" , suffix="" , Efermi=Efermi_Fe , comparer=compare_energyresult )
@@ -73,13 +78,25 @@ def test_Fe_wcc(system_Fe_W90_wcc, compare_energyresult,quantities_Fe,Efermi_Fe)
     check_integrate(system_Fe_W90_wcc , quantities_Fe , fout_name="berry_Fe_W90" , suffix="wcc" , Efermi=Efermi_Fe , comparer=compare_energyresult )
 
 
-def test_GaAs(system_GaAs_W90, compare_energyresult,quantities_GaAs,Efermi_GaAs):
+def test_GaAs(system_GaAs_W90,system_GaAs_tb ,compare_energyresult,quantities_GaAs,Efermi_GaAs):
     """Test berry dipole"""
     check_integrate(system_GaAs_W90 , quantities_GaAs , fout_name="berry_GaAs_W90" , suffix="" , Efermi=Efermi_GaAs , comparer=compare_energyresult ,
                   extra_precision = {"berry_dipole_fsurf":1e-6} )   # This is a low precision for the nonabelian thing, not sure if it does not indicate a problem, or is a gauge-dependent thing
+    check_integrate(system_GaAs_tb , quantities_GaAs , fout_name="berry_GaAs_tb" , suffix="" , Efermi=Efermi_GaAs , comparer=compare_energyresult ,
+                  extra_precision = {"berry_dipole_fsurf":1e-6} )   # This is a low precision for the nonabelian thing, not sure if it does not indicate a problem, or is a gauge-dependent thing
 
-def test_GaAs_wcc(system_GaAs_W90_wcc, compare_energyresult,quantities_GaAs,Efermi_GaAs):
+def test_GaAs_wcc(system_GaAs_W90_wcc,system_GaAs_tb_wcc , compare_energyresult,quantities_GaAs,Efermi_GaAs):
     """Test berry dipole with wcc_phase"""
     check_integrate(system_GaAs_W90_wcc , quantities_GaAs , fout_name="berry_GaAs_W90" , suffix="wcc" , Efermi=Efermi_GaAs , comparer=compare_energyresult ,
                   precision=1e-10 ,extra_precision = {"berry_dipole_fsurf":1e-6} )   # This is a low precision for the nonabelian thing, not sure if it does not indicate a problem
+    check_integrate(system_GaAs_tb_wcc , quantities_GaAs , fout_name="berry_GaAs_tb" , suffix="wcc" , Efermi=Efermi_GaAs , comparer=compare_energyresult ,
+                  extra_precision = {"berry_dipole_fsurf":1e-6} )   # This is a low precision for the nonabelian thing, not sure if it does not indicate a problem, or is a gauge-dependent thing
+
+def test_Haldane(system_Haldane_PythTB,system_Haldane_TBmodels,compare_energyresult,quantities_Haldane,Efermi_Haldane):
+    check_integrate(system_Haldane_PythTB , quantities_Haldane , fout_name="berry_Haldane_pythtb" , suffix="" , Efermi=Efermi_Haldane , comparer=compare_energyresult )
+    check_integrate(system_Haldane_TBmodels , quantities_Haldane , fout_name="berry_Haldane_tbmodels" , suffix="" , Efermi=Efermi_Haldane , comparer=compare_energyresult )
+
+def test_Haldane_wcc(system_Haldane_PythTB_wcc,system_Haldane_TBmodels_wcc,compare_energyresult,quantities_Haldane,Efermi_Haldane):
+    check_integrate(system_Haldane_PythTB_wcc , quantities_Haldane , fout_name="berry_Haldane_pythtb" , suffix="wcc" , Efermi=Efermi_Haldane , comparer=compare_energyresult )
+    check_integrate(system_Haldane_TBmodels_wcc , quantities_Haldane , fout_name="berry_Haldane_tbmodels" , suffix="wcc" , Efermi=Efermi_Haldane , comparer=compare_energyresult )
 
