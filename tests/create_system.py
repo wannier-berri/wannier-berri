c189e374b1a178210bfdeb0d093c2fef0e0c38c2
Stepan Tsirkin <stepan.tsirkin@uzh.ch>
2021-10-07 18:38:43 +0200

    moved models into a separate module

--- tests/create_system.py
+++ tests/create_system.py
@@ -4,12 +4,11 @@ import os
 import tarfile
 import shutil
 
-import tbmodels
-import pythtb 
 import pytest
 import numpy as np
 
 import wannierberri as wberri
+from wannierberri import models as wb_models
 
 from conftest import ROOT_DIR
 
@@ -173,24 +172,7 @@ def system_GaAs_tb_wcc():
 
 @pytest.fixture(scope="session")
 def tbmodels_Haldane():
-    delta=0.2
-    t=-1.0
-    t2 =0.15*np.exp((1.j)*np.pi/2.)
-    t2c=t2.conjugate()
-    my_model = tbmodels.Model(
-            on_site=[delta, -delta],uc = [[1.0,0.0],[0.5,np.sqrt(3.0)/2.0]], dim=2, occ=1, pos=[[1./3.,1./3.],[2./3.,2./3.]]
-            )
-    my_model.add_hop(t, 0, 1, [ 0, 0])
-    my_model.add_hop(t, 1, 0, [ 1, 0])
-    my_model.add_hop(t, 1, 0, [ 0, 1])
-    my_model.add_hop(t2 , 0, 0, [ 1, 0])
-    my_model.add_hop(t2 , 1, 1, [ 1,-1])
-    my_model.add_hop(t2 , 1, 1, [ 0, 1])
-    my_model.add_hop(t2c, 1, 1, [ 1, 0])
-    my_model.add_hop(t2c, 0, 0, [ 1,-1])
-    my_model.add_hop(t2c, 0, 0, [ 0, 1])
-    
-    return my_model
+    return wb_models.Haldane_tbm(delta=0.2,t=-1.0,t2 =0.15)
 
 @pytest.fixture(scope="session")
 def system_Haldane_TBmodels(tbmodels_Haldane):
@@ -212,28 +194,7 @@ def system_Haldane_TBmodels_internal(tbmodels_Haldane):
 
 @pytest.fixture(scope="session")
 def pythtb_Haldane():
-    lat=[[1.0,0.0],[0.5,np.sqrt(3.0)/2.0]]
-    orb=[[1./3.,1./3.],[2./3.,2./3.]]
-
-    my_model=pythtb.tb_model(2,2,lat,orb)
-
-    delta=0.2
-    t=-1.0
-    t2 =0.15*np.exp((1.j)*np.pi/2.)
-    t2c=t2.conjugate()
-
-    my_model.set_onsite([-delta,delta])
-    my_model.set_hop(t, 0, 1, [ 0, 0])
-    my_model.set_hop(t, 1, 0, [ 1, 0])
-    my_model.set_hop(t, 1, 0, [ 0, 1])
-    my_model.set_hop(t2 , 0, 0, [ 1, 0])
-    my_model.set_hop(t2 , 1, 1, [ 1,-1])
-    my_model.set_hop(t2 , 1, 1, [ 0, 1])
-    my_model.set_hop(t2c, 1, 1, [ 1, 0])
-    my_model.set_hop(t2c, 0, 0, [ 1,-1])
-    my_model.set_hop(t2c, 0, 0, [ 0, 1])
-    
-    return my_model
+    return wb_models.Haldane_ptb(delta=0.2,t=-1.0,t2 =0.15)
 
 
 @pytest.fixture(scope="session")
@@ -249,46 +210,8 @@ def system_Haldane_PythTB(pythtb_Haldane):
 
 @pytest.fixture(scope="session")
 def ChiralModel():
-    """Create a chiral model that also breaks time-reversal
-       can be used to test almost any quantity"""
-    delta=2
-    t1=1
-    hop2=1./3
-    phi=np.pi/10
-    hopz=0.2
-    lat=[[1.0,0.0,0.0],[0.5,np.sqrt(3.0)/2.0,0.0],[0.0,0.0,1.0]]
-    # define coordinates of orbitals
-    orb=[[1./3.,1./3.,0.0],[2./3.,2./3.,0.0]]
+    return wb_models.Chiral(delta=2, t1=1, hop2=1./3,  phi=np.pi/10, hopz=0.2)
 
-    # make tree dimensional (stacked) tight-binding Haldane model
-    haldane=pythtb.tb_model(3,3,lat,orb)
-
-    # set model parameters
-    t2=hop2*np.exp(1.j*phi)
-
-    # set on-site energies
-    haldane.set_onsite([-delta,delta])
-    # set hoppings (one for each connected pair of orbitals)
-    # from j in R to i in 0
-    # (amplitude, i, j, [lattice vector to cell containing j])
-    haldane.set_hop(t1, 0, 1, [ 0, 0,0])
-    haldane.set_hop(t1, 1, 0, [ 1, 0,0])
-    haldane.set_hop(t1, 1, 0, [ 0, 1,0])
-    # add second neighbour complex hoppings
-    haldane.set_hop(t2 , 0, 0, [  0,-1,0])
-    haldane.set_hop(t2 , 0, 0, [  1, 0,0])
-    haldane.set_hop(t2 , 0, 0, [ -1, 1,0])
-    haldane.set_hop(t2 , 1, 1, [ -1, 0,0])
-    haldane.set_hop(t2 , 1, 1, [  1,-1,0])
-    haldane.set_hop(t2 , 1, 1, [  0, 1,0])
-    # add chiral hoppings
-    haldane.set_hop(hopz  , 0, 0, [  0,-1,1])
-    haldane.set_hop(hopz  , 0, 0, [  1, 0,1])
-    haldane.set_hop(hopz  , 0, 0, [ -1, 1,1])
-    haldane.set_hop(hopz  , 1, 1, [ -1, 0,1])
-    haldane.set_hop(hopz  , 1, 1, [  1,-1,1])
-    haldane.set_hop(hopz  , 1, 1, [  0, 1,1])
-    return haldane
 
 
 @pytest.fixture(scope="session")
