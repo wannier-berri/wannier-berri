5f164f4b2fd06bc35a6c075b951227745630386c
Stepan Tsirkin <stepan@physik.uzh.ch>
2022-02-22 18:42:26 +0100

    * allowed the Chiral model to be left- or right-handed
    * test that flipping handedness reverses berry_dipole
    
    * test that TR flips AHCZ

--- tests/compare_result.py
+++ tests/compare_result.py
@@ -104,7 +104,8 @@ def compare_energyresult():
 @pytest.fixture
 def compare_any_result():
     """Compare dat file output of EnergyResult with the file in reference folder"""
-    def _inner(fout_name, suffix, adpt_num_iter,fout_name_ref = None,suffix_ref=None,compare_zero=False,precision=None,result_type=None):
+    def _inner(fout_name, suffix, adpt_num_iter,fout_name_ref = None,suffix_ref=None,compare_zero=False,precision=None,result_type=None,
+            flip_sign = False):
         if suffix_ref is None :
             suffix_ref=suffix
         if fout_name_ref is None :
@@ -122,12 +123,14 @@ def compare_any_result():
                 filename_ref = fout_name_ref + f"-{suffix_ref}_iter-{i_iter:04d}"+ext
                 path_filename_ref = os.path.join(REF_DIR, filename_ref)
                 result_ref = result_type(file_npz = path_filename_ref)
-                maxval = result_ref._maxval
+                if flip_sign : 
+                    result_ref *= -1
+                maxval = result_ref._maxval_raw
                 if precision is None:
                     precision = max(maxval / 1E12, 1E-11)
                 elif precision < 0:
                     precision = max(maxval * abs(precision) , 1E-11)
-            err = (result-result_ref)._maxval
+            err = (result-result_ref)._maxval_raw
             assert err < precision , error_message(
                 fout_name, suffix, i_iter, err, path_filename, path_filename_ref,precision)
     return _inner
