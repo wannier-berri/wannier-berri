ecd6ce0be8fdbf60256bc053b271513b9b3f4053
stepan-tsirkin <stepan.tsirkin@uzh.ch>
2022-02-25 12:52:49 +0100

    version update

--- wannierberri/__init__.py
+++ wannierberri/__init__.py
@@ -14,7 +14,7 @@
 wannierberri - a module for Wannier interpolation
 """
 
-__version__ = "0.11.1"
+__version__ = "0.11.1+"
 
 from .run import run
 from .__main import integrate,tabulate,integrate_options,tabulate_options,welcome,print_options
