3a8835e39ce115d1428a552e03e5ebe157ee9d0f
Jae-Mo Lihm <jaemolim96@gmail.com>
2022-04-29 00:03:00 +0900

    Apply yapf to __system_tb.py

--- wannierberri/__system_tb.py
+++ wannierberri/__system_tb.py
@@ -12,18 +12,18 @@
 #------------------------------------------------------------
 
 import numpy as np
-from termcolor import cprint 
+from termcolor import cprint
 from .__utility import real_recip_lattice
 from .__system import System
 
 
 class System_tb(System):
     """
-    System initialized from the `*_tb.dat` file, which can be written either by  `Wannier90 <http://wannier.org>`__ code, 
-    or composed by the user based on some tight-binding model. 
+    System initialized from the `*_tb.dat` file, which can be written either by  `Wannier90 <http://wannier.org>`__ code,
+    or composed by the user based on some tight-binding model.
     See Wannier90 `code <https://github.com/wannier-developers/wannier90/blob/2f4aed6a35ab7e8b38dbe196aa4925ab3e9deb1b/src/hamiltonian.F90#L698-L799>`_
-    for details of the format. 
-    
+    for details of the format.
+
     Parameters
     ----------
     tb_file : str
@@ -31,60 +31,57 @@ class System_tb(System):
 
     Notes
     -----
-    see also  parameters of the :class:`~wannierberri.System` 
+    see also  parameters of the :class:`~wannierberri.System`
     """
-
-    def __init__(self,tb_file="wannier90_tb.dat",**parameters):
+    def __init__(self, tb_file="wannier90_tb.dat", **parameters):
 
         self.set_parameters(**parameters)
-        if self.morb : raise ValueError("System_tb class cannot be used for evaluation of orbital magnetic moments")
-        if self.spin : raise ValueError("System_tb class cannot be used for evaluation of spin properties")
- 
-        self.seedname=tb_file.split("/")[-1].split("_")[0]
-        f=open(tb_file,"r")
-        l=f.readline()
-        cprint ("reading TB file {0} ( {1} )".format(tb_file,l.strip()),'green', attrs=['bold'])
-        real_lattice=np.array([f.readline().split()[:3] for i in range(3)],dtype=float)
-        self.real_lattice,self.recip_lattice= real_recip_lattice(real_lattice=real_lattice)
-        self.num_wann=int(f.readline())
-        nRvec=int(f.readline())
-        self.nRvec0=nRvec
-        self.Ndegen=[]
-        while len(self.Ndegen)<nRvec:
-            self.Ndegen+=f.readline().split()
-        self.Ndegen=np.array(self.Ndegen,dtype=int)
-        
-        self.iRvec=[]
-        
-        self.Ham_R=np.zeros( (self.num_wann,self.num_wann,nRvec) ,dtype=complex)
-        
+        if self.morb: raise ValueError("System_tb class cannot be used for evaluation of orbital magnetic moments")
+        if self.spin: raise ValueError("System_tb class cannot be used for evaluation of spin properties")
+
+        self.seedname = tb_file.split("/")[-1].split("_")[0]
+        f = open(tb_file, "r")
+        l = f.readline()
+        cprint("reading TB file {0} ( {1} )".format(tb_file, l.strip()), 'green', attrs=['bold'])
+        real_lattice = np.array([f.readline().split()[:3] for i in range(3)], dtype=float)
+        self.real_lattice, self.recip_lattice = real_recip_lattice(real_lattice=real_lattice)
+        self.num_wann = int(f.readline())
+        nRvec = int(f.readline())
+        self.nRvec0 = nRvec
+        self.Ndegen = []
+        while len(self.Ndegen) < nRvec:
+            self.Ndegen += f.readline().split()
+        self.Ndegen = np.array(self.Ndegen, dtype=int)
+
+        self.iRvec = []
+
+        self.Ham_R = np.zeros((self.num_wann, self.num_wann, nRvec), dtype=complex)
+
         for ir in range(nRvec):
             f.readline()
             self.iRvec.append(f.readline().split())
-            hh=np.array( [[f.readline().split()[2:4] 
-                             for n in range(self.num_wann)] 
-                                for m in range(self.num_wann)],dtype=float).transpose( (1,0,2) )
-            self.Ham_R[:,:,ir]=(hh[:,:,0]+1j*hh[:,:,1])/self.Ndegen[ir]
-        
-        self.iRvec=np.array(self.iRvec,dtype=int)
+            hh = np.array(
+                [[f.readline().split()[2:4] for n in range(self.num_wann)] for m in range(self.num_wann)],
+                dtype=float).transpose((1, 0, 2))
+            self.Ham_R[:, :, ir] = (hh[:, :, 0] + 1j * hh[:, :, 1]) / self.Ndegen[ir]
+
+        self.iRvec = np.array(self.iRvec, dtype=int)
 
         if self.getAA:
-            self.AA_R=np.zeros( (self.num_wann,self.num_wann,nRvec,3) ,dtype=complex)
+            self.AA_R = np.zeros((self.num_wann, self.num_wann, nRvec, 3), dtype=complex)
             for ir in range(nRvec):
                 f.readline()
-                assert (np.array(f.readline().split(),dtype=int)==self.iRvec[ir]).all()
-                aa=np.array( [[f.readline().split()[2:8] 
-                             for n in range(self.num_wann)] 
-                                for m in range(self.num_wann)],dtype=float)
-                self.AA_R[:,:,ir,:]=(aa[:,:,0::2]+1j*aa[:,:,1::2]).transpose( (1,0,2) ) /self.Ndegen[ir]
-            self.wannier_centers_cart_auto =  np.diagonal(self.AA_R[:,:,self.iR0,:],axis1=0,axis2=1).T 
-        else: 
+                assert (np.array(f.readline().split(), dtype=int) == self.iRvec[ir]).all()
+                aa = np.array(
+                    [[f.readline().split()[2:8] for n in range(self.num_wann)] for m in range(self.num_wann)],
+                    dtype=float)
+                self.AA_R[:, :, ir, :] = (aa[:, :, 0::2] + 1j * aa[:, :, 1::2]).transpose((1, 0, 2)) / self.Ndegen[ir]
+            self.wannier_centers_cart_auto = np.diagonal(self.AA_R[:, :, self.iR0, :], axis1=0, axis2=1).T
+        else:
             self.AA_R = None
-        
+
         f.close()
 
         self.do_at_end_of_init()
 
-        cprint ("Reading the system from {} finished successfully".format(tb_file),'green', attrs=['bold'])
-
-        
+        cprint("Reading the system from {} finished successfully".format(tb_file), 'green', attrs=['bold'])
