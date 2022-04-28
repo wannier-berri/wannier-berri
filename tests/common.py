6c730a4e66629ee7d45321d12278989d69f32efb
Jae-Mo Lihm <jaemolim96@gmail.com>
2022-04-28 22:03:29 +0900

    Apply yapf to common.py

--- tests/common.py
+++ tests/common.py
@@ -1,11 +1,10 @@
 """Common constants shared by all tests"""
 
 import os
-import pytest
 
 # Root folder containing test scripts
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Folder containing output dat files of tests
 OUTPUT_DIR = os.path.join(ROOT_DIR, "_dat_files")
-REF_DIR    = os.path.join(ROOT_DIR, 'reference')
+REF_DIR = os.path.join(ROOT_DIR, 'reference')
