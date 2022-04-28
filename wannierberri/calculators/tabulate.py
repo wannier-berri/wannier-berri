99e448b9058f8ce06fd49fea2d861d0e511d10b5
Jae-Mo Lihm <jaemolim96@gmail.com>
2022-04-29 00:17:22 +0900

    Apply yapf to calculators/tabulate.py

--- wannierberri/calculators/tabulate.py
+++ wannierberri/calculators/tabulate.py
@@ -1,11 +1,12 @@
 from .classes import Tabulator
 from wannierberri import covariant_formulak as frml
 
+
 class BerryCurvature(Tabulator):
-    def __init__(self,**kwargs):
-        super().__init__(frml.Omega,**kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(frml.Omega, **kwargs)
+
 
 class Energy(Tabulator):
-    def __init__(self,**kwargs):
-        super().__init__(frml.Eavln,**kwargs)
-
+    def __init__(self, **kwargs):
+        super().__init__(frml.Eavln, **kwargs)
