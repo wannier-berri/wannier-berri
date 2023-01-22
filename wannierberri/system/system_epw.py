eb89bf7246d57b54c35c06fe660d6f14474e9888
Stepan Tsirkin <stepan@physik.uzh.ch>
2023-01-22 20:00:44 +0100

    split System_Phonon class from the System_Phonon_qe

--- wannierberri/system/system_epw.py
+++ wannierberri/system/system_epw.py
@@ -5,6 +5,7 @@ from .system_phonon_qe import System_Phonon_QE
 from ..__utility import FFT,real_recip_lattice,FortranFileR
 import os
 
+# Note : it will probably not work if use_ws=True in EPW
 
 class System_EPW(System_w90):
 
@@ -25,27 +26,108 @@ class System_EPW(System_w90):
     """
 
     def __init__(self,seedname,
-            parameters_phonon={},
-            parameters_electron={},
+            system_electron=None,
+            system_phonon=None,
             **parameters):
 
         self.set_parameters(**parameters)
         path = tuple(seedname.split("/")[:-1])
         path = os.path.join(*path) if len(path)>0 else ""
-        f  = open(os.path.join(path,"epwdata.fmt"),"r")
-        ef = float(f.readline())
-        nbndsub, nrr_k, nmodes, nrr_q, nrr_g = (int(x) for x in f.readline().split())
-#        zstar, epsi = (float(x) for x in f.readline().split())
-        f.close()
+        read_crystal(os.path.join(path,"crystal.fmt"))
+        (nbndsub, nmodes, nrr_k, nrr_q, nrr_g, Ham_R, dyn_mat, irvec_k, irvec_q, irvec_g,
+            ndegen_k,ndegen_g) = read_epwdata(os.path.join(path,"epwdata.fmt"))
         with open(seedname+".epmatwp","rb") as f:
             epmatwp = np.fromfile(f,dtype=np.complex128).reshape( (nbndsub, nbndsub, nrr_k, nmodes,nrr_g), order='F' )
+        epmatwp/=ndegen_k[None,None,:,None,None]
+        epmatwp/=ndegen_g[None,None,None,None,:]
+        print (nbndsub,nmodes,nrr_k,nrr_q,nrr_g)
 
-        self.system_ph=System_Phonon_QE(seedname,**parameters_phonon)
-        self.system_el=System_w90(seedname,**parameters_electron)
+#        self.system_ph=System_Phonon_QE(seedname,**parameters_phonon)
+#        self.system_el=System_w90(seedname,**parameters_electron)
 
-
+# os.path.join(path,"epwdata.fmt"),"r")
 
     @property
     def is_electron_phonon(self):
         return True
 
+
+
+class FormattedFile():
+
+    def __init__(self,filename):
+        self.fl = open(filename,"r")
+
+    def read_dtype(self,dtype):
+        return [dtype(x) for x in self.fl.readline().split()]
+
+    def readint(self):
+        return self.read_dtype(int)
+
+    def readcomplex(self):
+        return np.complex(*np.safe_eval(self.fl.readline().strip()))
+
+    def readfloat(self):
+        return self.read_dtype(float)
+
+    def readline(self):
+        return self.fl.readline()
+
+def read_epwdata(filename):
+    
+    f  = FormattedFile(filename)
+    # so far just read lines, data will be extracted when needed
+    f.readfloat() # WRITE(epwdata,*) ef
+    nbndsub, nrr_k, nmodes, nrr_q, nrr_g = f.readint() # WRITE(epwdata,*) nbndsub, nrr_k, nmodes, nrr_q, nrr_g
+    f.readline() # WRITE(epwdata,*) zstar, epsi
+    # TODO : optimize reading (try sdsolutions from here : https://stackoverflow.com/questions/34882397/read-fortran-complex-number-into-python
+    Ham_R = np.zeros( (nbndsub,nbndsub,nrr_k), dtype=complex)
+    for ibnd in range(nbndsub):
+        for jbnd in range(nbndsub):
+            for irk in range(nrr_k):
+                Ham_R[ibnd,jbnd,irk] = f.readcomplex() # WRITE (epwdata,*) chw(ibnd, jbnd, irk)
+    dyn_mat = np.zeros( (nmodes,nmodes,nrr_q), dtype=complex)
+    for imode in range(nmodes):
+        for jmode in range(nmodes):
+            for irq in range(nrr_q):
+                dyn_mat[imode,jmode,irq] = f.readcomplex()  # WRITE(epwdata,*) rdw(imode, jmode, irq)
+    irvec_k = np.zeros( (nrr_k,3),dtype = int)
+    ndegen_k = np.zeros( nrr_k,dtype = int) 
+    for irk in range(nrr_k):
+        _ = f.readint()        #    WRITE(epwdata,*) irvec_k(:,irk), ndegen_k(irk)
+        irvec_k[irk],ndegen_k[irk] = _[:3],_[3]
+    irvec_q = np.zeros( (nrr_q,3),dtype = int)
+    ndegen_q = np.zeros( nrr_q,dtype = int) 
+    for irq in range(nrr_q):
+        _ = f.readint()        #    WRITE(epwdata,*) irvec_q(:,irq), ndegen_q(irq)
+        irvec_q[irq],ndegen_q[irq] = _[:3],_[3]
+    irvec_g = np.zeros( (nrr_g,3),dtype = int)
+    ndegen_g = np.zeros( nrr_g,dtype = int) 
+    for irg in range(nrr_g):
+        _ = f.readint()        #    WRITE(epwdata,*) irvec_g(:,irg), ndegen_g(irg)
+        irvec_g[irg],ndegen_g[irg] = _[:3],_[3]
+    assert np.all(ndegen_k>0)
+    assert np.all(ndegen_q>0)
+    assert np.all(ndegen_g>0)
+    Ham_R = Ham_R/ndegen_k[None,None,:]
+    dyn_mat = dyn_mat/ndegen_q[None,None,:]
+    return (nbndsub, nmodes, nrr_k, nrr_q, nrr_g, 
+                Ham_R, dyn_mat,
+                irvec_k, irvec_q, irvec_g, ndegen_k, ndegen_g)
+
+
+def read_crystal(filename):
+    f  = open(filename,"r")
+    f.readline() #  WRITE(crystal,*) nat
+    f.readline() #  WRITE(crystal,*) nmodes
+    f.readline() #  WRITE(crystal,*) nelec
+    f.readline() #  WRITE(crystal,*) at
+    f.readline() #  WRITE(crystal,*) bg
+    f.readline() #  WRITE(crystal,*) omega
+    f.readline() #  WRITE(crystal,*) alat
+    f.readline() #  WRITE(crystal,*) tau
+    f.readline() #  WRITE(crystal,*) amass
+    f.readline() #  WRITE(crystal,*) ityp
+    f.readline() #  WRITE(crystal,*) noncolin
+    f.readline() #  WRITE(crystal,*) w_centers
+    return None
---
+++ wannierberri/system/system_phonon.py
@@ -0,0 +1,88 @@
+import numpy as np
+import untangle
+import multiprocessing
+from wannierberri.__utility import real_recip_lattice
+from .system import System
+from wannierberri.__utility import FFT
+from scipy import constants as const
+from ..__factors import Ry_eV
+
+#from scipy.constants import elementary_charge, hbar, electron_mass, physical_constants, angstrom  #, Boltzmann
+
+#print ( 1/(hbar*2*np.pi*1e12/elementary_charge) )
+
+# These constants are taken from QE
+AMU_RY = const.m_u/const.m_e/2
+
+def _str2array(s,dtype=float):
+    if dtype == float:
+        return np.array([_.split() for _ in s.strip().split("\n")],dtype=float)
+    elif dtype == complex:
+        s = s.replace(","," ")
+        s = _str2array(s)
+        return s[:,0]+1j*s[:,1]
+    else:
+        raise ValueError(f"dtype = '{dtype}' is not supported by _str2array")
+
+
+class System_Phonon(System):
+
+    """Class to represent dynamical matrices from anything"""
+
+    def __init__(self,
+                real_lattice,
+                masses,
+                iRvec,ndegen=None,
+                mp_grid=None,
+                Ham_R=None,dyn_mat_R=None,
+                atom_positions_cart=None,atom_positions_red=None, # speciy one of them
+                asr=True,
+                ):
+
+        self.mp_grid = mp_grid
+        self.real_lattice, self.recip_lattice = real_recip_lattice(real_lattice=real_lattice)
+        assert (atom_positions_red is None)!=(atom_positions_cart is None), "need to specify either atom_positions_red or atom_positions_cart, but never both) "
+        if atom_positions_red is not None:
+            self.atom_positions_red = atom_positions_red
+        else:
+            self.atom_positions_red = atom_positions_cart.dot(np.linalg.inv(self.real_lattice))
+        self.number_of_atoms = self.atom_positions_red.shape[0]
+        self.num_wann = 3*self.number_of_atoms
+        self.wannier_centers_red = np.array( [atom for atom in self.atom_positions_red for i in range(3)])
+        self.wannier_centers_cart = self.wannier_centers_red.dot(self.real_lattice)
+        self.iRvec=iRvec
+        self.masses = masses
+        assert (Ham_R is None)!=(dyn_mat_R is None), "need to specify either dyn_mat_R or Ham_R, but never both) "
+        if Ham_R is not None:
+            self.Ham_R = Ham_R
+        else:
+            self.Ham_R = dyn_mat_R*(Ry_eV**2) # now the units are eV**2, to be "kind of consistent" with electronic systems
+            for i in range(self.number_of_atoms):
+                for j in range(self.number_of_atoms):
+                    self.Ham_R[3*i:3*i+3, 3*j:3*j+3, :] /= np.sqrt(masses[i]*masses[j]) * AMU_RY
+        if ndegen is not None:
+            assert np.all(ndegen>0)
+            self.Ham_R/=ndegen[None,None,:]
+        if asr :
+            self.acoustic_sum_rule()
+        self.do_at_end_of_init()
+
+
+
+    def acoustic_sum_rule(self,apply_masses = True):
+        iR0 = self.iR0
+        for i in range(self.number_of_atoms):
+            for j in range(self.number_of_atoms):
+                self.Ham_R[3*i:3*i+3, 3*j:3*j+3, :] *= np.sqrt(self.masses[i]*self.masses[j]) 
+        for i in range(3):
+            for j in range(3):
+                for a in range(self.number_of_atoms):
+                    self.Ham_R[3*a+i,3*a+j,iR0]-= self.Ham_R[3*a+i,j::3,:].sum()
+        for i in range(self.number_of_atoms):
+            for j in range(self.number_of_atoms):
+                self.Ham_R[3*i:3*i+3, 3*j:3*j+3, :] /= np.sqrt(self.masses[i]*self.masses[j]) 
+
+    @property
+    def is_phonon(self):
+        return True
+
