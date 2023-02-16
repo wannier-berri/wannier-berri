e48275e99d5f4b04cb2efcccf0b6cd4c449108cd
Stepan Tsirkin <stepan@physik.uzh.ch>
2023-02-16 15:45:38 +0100

    fix lint

--- wannierberri/evaluate_k.py
+++ wannierberri/evaluate_k.py
@@ -2,8 +2,8 @@ import numpy as np
 from collections.abc import Iterable
 from .__grid import Grid
 from .data_K import Data_K
-from .calculators import Calculator, tabulate
 from collections import defaultdict
+from .calculators import tabulate
 
 available_quantities = {
     "energy": tabulate.Energy(print_comment=False),
@@ -27,15 +27,14 @@ def help():
     return hlp
 
 
-def evaluate_k(
-                system = None,
+def evaluate_k( system=None,
                 k=[0.,0.,0.],
-                quantities = [],
-                calculators = {},
-                formula = {},
-                param_formula = {},
-                iband = None,
-                return_single_as_dict = False
+                quantities=[],
+                calculators={},
+                formula={},
+                param_formula={},
+                iband=None,
+                return_single_as_dict=False
               ):
     """This function presents a shortcut to evaluate some property at a particular k-point
     The main goal is to be convenient, rather than efficient
@@ -44,13 +43,13 @@ def evaluate_k(
     ----------
     quantities : list of str
         Which qpre-defined quantities to calculate. Use `evaluate()` to get the list of available quantities
-    calculators : dict 
+    calculators : dict
         Dictionary str : :class:`~wanierberri.calculators.Calculator`
         alows to evaluate quantities that are not available here, or using specific parameters
-    formula : dict 
+    formula : dict
         Dictionary str : :class:`~wanierberri.formula.Formula_ln`
         alows to evaluate block-diagonal part of a covariant formula (considering iband as "inner" states)
-    param_formula : dict 
+    param_formula : dict
         Dictionary str : dict -parameters to be passed to the corresponding formula
     return_single_as_dict : bool
         wether to pack a result into dict if only one calculator/quantity is requested
@@ -63,7 +62,7 @@ def evaluate_k(
         without packing into dict
     """
 
-   # without parameters - just print the available options
+    # without parameters - just print the available options
     if system is None:
         print (__doc__)
         print ( help() )
@@ -86,7 +85,7 @@ def evaluate_k(
     if not isinstance(iband,Iterable):
         iband = [iband]
     iband_out = sorted(np.array(list(set(range(system.num_wann))-set(iband))))
-    
+
 
     # Now get the data at the single k-point
     #result_data = {}
@@ -104,7 +103,7 @@ def evaluate_k(
 
     _param_formula = defaultdict(lambda : {} )
     _param_formula.update(param_formula)
-    
+
     for k,f in formula.items():
         form = f(data_k,**_param_formula[k])
         result[k] = form.nn(0,iband,iband_out)
