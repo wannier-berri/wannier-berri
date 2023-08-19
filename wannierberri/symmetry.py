b22f765aa83135928af4013fccc861802d39a71b
Stepan Tsirkin <stepan@physik.uzh.ch>
2023-08-19 14:00:11 +0200

    simplified Transform, added docs

--- wannierberri/symmetry.py
+++ wannierberri/symmetry.py
@@ -53,8 +53,6 @@ from copy import deepcopy
 from .__utility import real_recip_lattice
 from collections.abc import Iterable
 
-import abc
-
 SYMMETRY_PRECISION = 1e-6
 
 
@@ -63,6 +61,14 @@ class Symmetry():
     Symmetries that acts on reciprocal space objects, in Cartesian coordinates.
     A k-point vector ``k`` transform as ``self.iTR * self.iInv * (sym.R @ k)``.
 
+    Parameters
+    ------------
+    R : (3, 3) ndarray
+        Rotation matrix.  ``det(R) = 1 or -1``.
+    TR : bool
+        True if symmetry involves time reversal.
+
+
     Attributes
     ----------
     R : (3, 3) ndarray
@@ -70,7 +76,7 @@ class Symmetry():
     TR : bool
         True if symmetry involves time reversal.
     Inv : bool
-        True if symmetry involves spatial inversion.
+        True if symmetry involves spatial inversion. (i.e. if on input det(R) was -1)
     """
 
     def __init__(self, R, TR=False):
@@ -369,199 +375,80 @@ class Group():
 
 
 
-class Transform(abc.ABC):
+class Transform():
+    r"""Describes transformation of a tensor under inversion or time-reversal
 
+    Parameters
+    ----------
+    factor : int
+        mu,tiplication fuctor (+1 or -1)
+    conj : bool
+        apply complex conjugation
+    transpose_axes : tuple
+        how to permute the axes of the tensor.
 
-    def __init__(self,dimension=None):
-        """if dimension is not None - it is the only number of cartesian indices
-            in the tensor alowed for the transfoprmation
+    Note
+    -----
+    * Pre-defined transforms are :
+        + `transform_ident = Transform()`
+        + `transform_odd   = Transform(factor=-1)`
+        + `transform_trans = Transform(transpose_axes=(1,0))`
         """
-        self.dimension=dimension
 
-    @abc.abstractmethod
+
+    def __init__(self,factor=1,conj=False,transpose_axes=None):
+        self.conj=conj
+        self.factor=factor
+        assert factor in (1,-1),f"factor is {factor}"
+        self.transpose_axes=transpose_axes
+
+    def __str__(self):
+        return (f"Transform(factor={self.factor}, conj={self.conj}, transpose_axes={self.transpose_axes}")
+
     def __call__(self,res):
-        pass
+        if self.transpose_axes is not None:
+            dim0=res.ndim-len(self.transpose_axes)
+            trans = tuple(i for i in range(dim0))+tuple(dim0+a for a in self.transpose_axes)
+            res[:]=res.transpose(trans)
+        if self.conj:
+            res[:]=res[:].conj()
+        res[:]*=self.factor
+        return res
 
     def __eq__(self,other):
-        verbose = False  # True for testing
+        verbose = True  # True for testing
         if verbose:
             print (f"checking equaality of {self} and {other}")
         if not isinstance(other,Transform):
             if verbose: print (f"the other is not Transform, it is {other}")
             return False
-        elif self.dimension!=other.dimension :
-            if verbose: print (f"dimensions do not fit : {self.dimension} and {other.dimension}")
-            return False
-        else:
-            dim = self.dimension
-            if dim is None:
-                dim=2
-            res1 = random_tensors[dim]
-            res2 = np.copy(res1)
-            self(res1)
-            other(res2)
-            check = np.max( abs(res1-res2) )
-            if check> 1e-8:
+        for key in "factor","conj","transpose_axes":
+            if getattr(self,key)!=getattr(other,key):
                 if verbose:
-                    print ("the tensors after transformation are: \n{res1}\n and \n {res2} \n they differ by {check}")
+                    print (f"are NOT equal becasuse of {key} : {getattr(self,key)}!={getattr(other,key)}")
                 return False
-            else:
-                if verbose:
-                    print (f"are equal : {self} and {other}")
-                return True
+        if verbose:
+            print (f"are equal : {self} and {other}")
+        return True
 
-
-class TransformIdent(Transform):
-
-    def __call__(self,res):
-        pass
-
-    def __str__(self):
-        return ("TransformIdent")
-
-class TransformOdd(Transform):
-
-    def __call__(self,res):
-        res[:]=-res[:]
-
-    def __str__(self):
-        return ("TransformOdd")
-
-class TransformTrans(Transform):
-
-
-    def __init__(self,axes=(1,0)):
-        """if dimension is not None - it is the only number of cartesian indices
-            in the tensor alowed for the transfoprmation
-        """
-        self.axes=tuple(axes)
-        self.dimension=len(axes)
-
-    def __call__(self,res):
-        dim0=res.ndim-self.dimension
-        trans = tuple(i for i in range(dim0))+tuple(dim0+a for a in self.axes)
-        res[:]=res.transpose(trans)
-
-    def __str__(self):
-        return (f"TransformTrans({self.axes})")
-
-
-transform_ident = TransformIdent()
-transform_odd   = TransformOdd()
-transform_trans = TransformTrans()
 
 
 class TransformProduct(Transform):
+    """Constructs a :class:`~Transform`
+    from a list of :class:`~Transform`
+    """
 
     def __init__(self, transform_list):
-        self.transform_list=list(transform_list)
-        dimensions = list([t.dimension for t in self.transform_list])
-        if None in dimensions:
-            if len(set(dimensions))!=1 :
-                raise ValueError("either ALL of NONE of the transformatrions in a product should have dimension . found {dimensions}")
-            self.dimension = None
-        else:
-            self.dimension = sum(dimensions)
-
-    def __call__(self,res):
-        for tr in self.transform_list:
-            tr(res)
-
-    def __str__(self):
-        return "Product of [" + ", ".join(str(t) for t in self.transform_list) + " ]"
+        transform_list=list(transform_list)
+        conj_list = list([t.conj for t in transform_list])
+        if len(set(conj_list))!=1 :
+            raise ValueError("either ALL of NONE of the transformatrions in a product should have conjugation .  {conj_list}")
+        if np.any([t.transpose_axes is not None for t in transform_list]):
+            raise NotImplementedError("Product of transformations including transposing is not implemented")
+        super().__init__(factor=np.prod([t.factor for t in transform_list]), conj=conj_list[0])
 
 
-random_tensors = { 0:np.array([0.80596238+0.34296245j]),
-        1: np.array([0.25071929+0.34491236j, 0.04877657+0.17022553j,
-         0.39690414+0.50507555j]), 
-        2: np.array([[0.470442  +0.95357786j, 0.49501982+0.9856977j ,
-         0.70029476+0.22566716j],
-        [0.14017942+0.0417328j , 0.67810481+0.91156402j,
-         0.99808451+0.2213261j ],
-        [0.65149989+0.96146194j, 0.01377349+0.01784413j,
-         0.80596238+0.34296245j]]), 
-        3: np.array([[[0.72843698+0.60452792j, 0.26848089+0.5645207j ,
-         0.5894163 +0.62908298j],
-        [0.70418612+0.65981844j, 0.50711601+0.2101012j ,
-         0.97623005+0.94589939j],
-        [0.34623831+0.42267871j, 0.13230829+0.60132712j,
-         0.17269701+0.66593089j]],
-
-       [[0.62252721+0.25972703j, 0.38588518+0.44633076j,
-         0.26748758+0.82460679j],
-        [0.20973642+0.87918606j, 0.35768832+0.36513199j,
-         0.35634033+0.32618218j],
-        [0.41633665+0.30948491j, 0.38232737+0.61776951j,
-         0.68191988+0.28190326j]],
-
-       [[0.82881472+0.19917847j, 0.5504073 +0.4946653j ,
-         0.07215705+0.97850916j],
-        [0.99621333+0.38722484j, 0.97585631+0.5840961j ,
-         0.22429663+0.14430257j],
-        [0.84854584+0.27526566j, 0.03537387+0.13132689j,
-         0.98373851+0.06963218j]]]), 
-        4: np.array([[[[0.30031749+0.68469709j, 0.58659937+0.24525988j,
-          0.0597766 +0.05075574j],
-         [0.12479131+0.90183041j, 0.80639707+0.66720799j,
-          0.51740499+0.04500371j],
-         [0.29795983+0.66040221j, 0.60433079+0.65185754j,
-          0.3298667 +0.60153711j]],
-
-        [[0.7112296 +0.9846345j , 0.95713785+0.20224548j,
-          0.99630118+0.37702615j],
-         [0.97142677+0.97842682j, 0.8466332 +0.40146803j,
-          0.89224514+0.78101255j],
-         [0.05634177+0.45136221j, 0.3366071 +0.04759113j,
-          0.18878582+0.60663714j]],
-
-        [[0.76642002+0.3875563j , 0.2775941 +0.48162047j,
-          0.52805749+0.0800852j ],
-         [0.44986335+0.83752967j, 0.3317307 +0.87730627j,
-          0.00941376+0.76739382j],
-         [0.21629705+0.63123142j, 0.53396577+0.88771443j,
-          0.13835306+0.76693498j]]],
-
-
-       [[[0.74012236+0.59049934j, 0.87205772+0.53257876j,
-          0.53048185+0.75441839j],
-         [0.90906743+0.36159884j, 0.85192814+0.00565488j,
-          0.2228864 +0.54438124j],
-         [0.05848741+0.23302882j, 0.07905387+0.57442505j,
-          0.07725599+0.94095147j]],
-
-        [[0.6836054 +0.12826987j, 0.79610868+0.02788082j,
-          0.37195177+0.26364243j],
-         [0.64980615+0.23827312j, 0.27448158+0.68271081j,
-          0.52409896+0.64693385j],
-         [0.87922886+0.17594415j, 0.75541579+0.19611622j,
-          0.07322409+0.42616649j]],
-
-        [[0.26586886+0.38057158j, 0.04297439+0.04077598j,
-          0.01259778+0.94651108j],
-         [0.40609943+0.8285334j , 0.45170852+0.71981179j,
-          0.32755724+0.74186577j],
-         [0.93301137+0.78309037j, 0.94662744+0.14698388j,
-          0.73885154+0.80563412j]]],
-
-
-       [[[0.85501232+0.52963992j, 0.78767093+0.34986533j,
-          0.52119841+0.78410604j],
-         [0.39439026+0.56389553j, 0.36449086+0.4192848j ,
-          0.86797015+0.19484826j],
-         [0.97773862+0.2100186j , 0.10800484+0.48067086j,
-          0.19862437+0.90569614j]],
-
-        [[0.10677531+0.57929921j, 0.12707442+0.67470644j,
-          0.4516258 +0.62156642j],
-         [0.13300833+0.55650137j, 0.40436296+0.5847801j ,
-          0.45804265+0.71206061j],
-         [0.10762864+0.77615143j, 0.90595254+0.55383336j,
-          0.68232845+0.6914805j ]],
-
-        [[0.17390251+0.84810437j, 0.78534032+0.55692283j,
-          0.39799596+0.16364989j],
-         [0.80922176+0.38966705j, 0.44245086+0.34459141j,
-          0.43973888+0.46929441j],
-         [0.40355698+0.93692636j, 0.78189567+0.84883019j,
-          0.02322222+0.28217111j]]]])}
+transform_ident = Transform()
+transform_odd   = Transform(factor=-1)
+transform_trans = Transform(transpose_axes=(1,0))
 
