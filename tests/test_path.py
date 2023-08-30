867d213feaf94a6956736270e43b6b4ccf0d2602
Stepan Tsirkin <stepan@physik.uzh.ch>
2023-08-31 01:42:04 +0200

    fixed test_path

--- tests/test_path.py
+++ tests/test_path.py
@@ -4,9 +4,11 @@ import numpy as np
 from pytest import approx
 
 import wannierberri as wberri
+from wannierberri.calculators import tabulate as caltab
+from wannierberri.calculators import TabulatorAll
 
 from common import OUTPUT_DIR, REF_DIR
-
+from test_run import check_run
 
 def test_path_1(system_Haldane_PythTB):
     # Test the construction of Path class
@@ -62,92 +64,55 @@ def test_path_4(system_Haldane_PythTB):
     assert path_npy.K_list == approx(path.K_list), "path.K_list is wrong"
 
 
-def test_tabulate_path(system_Haldane_PythTB):
+def test_tabulate_path(system_Haldane_PythTB, check_run):
+    param_tab = {'degen_thresh': 5e-2,}
+    no_external = dict(kwargs_formula={"external_terms": False }, )
 
-
-def test_Fe(check_run, system_Fe_W90, compare_any_result, compare_fermisurfer):
-    param = {'Efermi': Efermi_Fe}
-    param_tab = {'degen_thresh': 5e-2}
-    calculators = {k: v(**param) for k, v in calculators_Fe.items()}
-    quantities = ['V', 'berry', 'Der_berry', 'morb', 'Der_morb']
-    calculators["tabulate"] = calc.TabulatorAll(
+    calculators = {}
+    calculators["tabulate"] = TabulatorAll(
         {
-            "Energy": calc.tabulate.Energy(),  # yes, in old implementation degen_thresh was applied to qunatities,
+            "Energy": caltab.Energy(),  # yes, in old implementation degen_thresh was applied to qunatities,
             # but not to energies
-            "V": calc.tabulate.Velocity(**param_tab),
-            "Der_berry": calc.tabulate.DerBerryCurvature(**param_tab),
-            "berry": calc.tabulate.BerryCurvature(**param_tab),
-            'morb': calc.tabulate.OrbitalMoment(**param_tab),
-            'Der_morb': calc.tabulate.DerOrbitalMoment(**param_tab),
+            "V": caltab.Velocity(**param_tab),
+            "Der_berry": caltab.DerBerryCurvature(**param_tab, **no_external),
+            "berry": caltab.BerryCurvature(**param_tab, **no_external),
+            'morb': caltab.OrbitalMoment(**param_tab, **no_external),
+            'Der_morb': caltab.DerOrbitalMoment(**param_tab, **no_external),
         },
-        ibands=[5, 6, 7, 8])
+        ibands=[0],
+        mode = "path"
+        )
+
+
+    k_nodes = [[0.0, 0.0, 0.5], [0.0, 0.0, 0.0], [0.5, 0.5, 0.5]]
+    path = wberri.Path(system_Haldane_PythTB, k_nodes=k_nodes, dk=1.0)
+
 
     result = check_run(
-        system_Fe_W90,
-        calculators,
+        system=system_Haldane_PythTB,
+        grid=path,
+        calculators=calculators,
         fout_name="berry_Fe_W90",
         suffix="run",
         parameters_K={
             '_FF_antisym': True,
             '_CCab_antisym': True
         },
-        extra_precision={"Morb": -1e-6},
-        skip_compare=['tabulate', 'opt_conductivity', 'opt_SHCqiao', 'opt_SHCryoo'])
-
-    for quant in 'opt_conductivity', 'opt_SHCryoo', 'opt_SHCryoo':
-        compare_any_result(
-            "berry_Fe_W90",
-            quant + "-run",
-            0,
-            fout_name_ref="kubo_Fe_W90",
-            suffix_ref=quant,
-            precision=-1e-8,
-            result_type=EnergyResult)
-
-    extra_precision = {'Morb': 1e-6, 'Der_berry':5e-8}
-    for quant in result.results.get("tabulate").results.keys(): # ["Energy", "berry","Der_berry","spin","morb"]:
-        for comp in result.results.get("tabulate").results.get(quant).get_component_list():
-            _quant = "E" if quant == "Energy" else quant
-            _comp = "-" + comp if comp != "" else ""
-            #            data=result.results.get(quant).data
-            #            assert data.shape[0] == len(Efermi)
-            #            assert np.all( np.array(data.shape[1:]) == 3)
-            prec = extra_precision[quant] if quant in extra_precision else 2e-8
-            #            comparer(frmsf_name, quant+_comp+suffix,  suffix_ref=compare_quant(quant)+_comp+suffix_ref ,precision=prec )
-            compare_fermisurfer(
-                fout_name="berry_Fe_W90-tabulate",
-                suffix=_quant + _comp + "-run",
-                suffix_ref=_quant + _comp,
-                fout_name_ref="tabulate_Fe_W90",
-                precision=prec)
-
-
-
-    quantities = ['V', 'berry', 'Der_berry', 'morb', 'Der_morb']
-
-    k_nodes = [[0.0, 0.0, 0.5], [0.0, 0.0, 0.0], [0.5, 0.5, 0.5]]
-    path = wberri.Path(system_Haldane_PythTB, k_nodes=k_nodes, dk=1.0)
-
-    tab_result = wberri.tabulate(
-        system=system_Haldane_PythTB,
-        grid=path,
-        quantities=quantities,
-        user_quantities={},
-        parameters={"external_terms": False},
-        #                    specific_parameters = specific_parameters,
-        ibands=[0],
-        use_irred_kpt=True,
-        symmetrize=True,  # should have no effect, but will check the cases and give a warning
+        use_symmetry=True, # should have no effect, but will check the cases and give a warning
         #                parameters_K = parameters_K,
         #                frmsf_name = None,
         #                degen_thresh = degen_thresh, degen_Kramers = degen_Kramers
-    )
+        skip_compare=['tabulate', ])
+
+
+    tab_result = result.results["tabulate"]
 
     filename = "path_tab.pickle"
     fout = open(os.path.join(OUTPUT_DIR, filename), "wb")
 
     data = {}
-    for quant in ["Energy"] + quantities:
+    quantities = result.results["tabulate"].results.keys()
+    for quant in quantities:
         result_quant = tab_result.results.get(quant)
         for comp in result_quant.get_component_list():
             data[(quant, comp)] = result_quant.get_component(comp)
@@ -155,7 +120,7 @@ def test_Fe(check_run, system_Fe_W90, compare_any_result, compare_fermisurfer):
 
     data_ref = pickle.load(open(os.path.join(REF_DIR, filename), "rb"))
 
-    for quant in ["Energy"] + quantities:
+    for quant in quantities:
         for comp in tab_result.results.get(quant).get_component_list():
             _data = data[(quant, comp)]
             _data_ref = data_ref[(quant, comp)]
