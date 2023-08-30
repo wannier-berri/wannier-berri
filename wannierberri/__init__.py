8626997368c2e5e5fccb2fa4231e2ea85b2b25ee
Stepan Tsirkin <stepan@physik.uzh.ch>
2023-08-31 00:01:22 +0200

    source code is now uploaded to pypi

--- wannierberri/__init__.py
+++ wannierberri/__init__.py
@@ -14,7 +14,7 @@
 wannierberri - a module for Wannier interpolation
 """
 
-__version__ = "0.14.0"
+__version__ = "0.14.1"
 
 from .run import run
 from .__old_API.__main import integrate, tabulate #, integrate_options, tabulate_options, print_options
