76634e79665ef8851564805a56db115abd998508
Oscar <oscarpoz.oca@gmail.com>
2024-01-10 11:38:39 +0100

    nspin=2 case in interface PythTB -> WB
    Incorporate the interface with nspin=2 according to the following PR:
    
    https://github.com/wannier-berri/wannier-berri/pull/299

--- wannierberri/system/system_tb_py.py
+++ wannierberri/system/system_tb_py.py
@@ -50,14 +50,17 @@ class System_tb_py(System):
             Rvec = np.array([R[0] for R in model.hop.items()], dtype=int)
         elif module == 'pythtb':
             real = model._lat
-            self.num_wann = model._norb
+            positions = model._orb
             if model._nspin == 1:
                 self.spinors = False
+                self.num_wann = mode._norb
             elif model._nspin == 2:
                 self.spinors = True
+                self.num_wann = model._norb * 2
+                positions = np.array(sum(([p, p] for p in positions), []))
             else:
                 raise Exception("\n\nWrong value of nspin!")
-            positions = model._orb
+            print("number of wannier functions:", self.num_wann)
             Rvec = np.array([R[-1] for R in model._hoppings], dtype=int)
         else:
             raise ValueError("unknown tight-binding module {}".format(module))
@@ -112,13 +115,24 @@ class System_tb_py(System):
             for nhop in model._hoppings:
                 i = nhop[1]
                 j = nhop[2]
-                iR = np.argwhere(np.all((nhop[-1] - self.iRvec[:, :self.dimr]) == 0, axis=1))
-                inR = np.argwhere(np.all((-nhop[-1] - self.iRvec[:, :self.dimr]) == 0, axis=1))
-                Ham_R[i, j, iR] += nhop[0]
-                Ham_R[j, i, inR] += np.conjugate(nhop[0])
+                iR = np.argwhere(np.all((nhop[-1] - self.iRvec[:, :self.dimr]) == 0, axis=1))[0][0]
+                inR = np.argwhere(np.all((-nhop[-1] - self.iRvec[:, :self.dimr]) == 0, axis=1))[0][0]
+                if model._nspin == 1:
+                    Ham_R[i, j, iR] += nhop[0]
+                    Ham_R[i, j, inR] += np.conjugate(nhop[0])
+                elif model._nspin == 2:
+                    print("hopping :", nhop[0].shape, Ham_R.shape, iR,
+                              Ham_R[2 * i:2 * i + 2, 2 * j:2 * j + 2, iR].shape)
+                    Ham_R[2 * i:2 * i + 2, 2 * j:2 * j + 2, iR] += nhop[0]
+                    Ham_R[2 * j:2 * j + 2, 2 * i:2 * i + 2, inR] += np.conjugate(nhop[0].T)
+
             # Set the onsite energies at H(R=[000])
             for i in range(model._norb):
-                Ham_R[i, i, index0] = model._site_energies[i]
+                if model._nspin == 1:
+                    Ham_R[i, i, index0] = model._site_energies[i]
+                elif model._nspin == 2:
+                    Ham_R[2 * i:2 * i + 2, 2 * i:2 * i + 2, index0] = model._site_energies[i]
+
         self.set_R_mat('Ham',Ham_R)
 
 
