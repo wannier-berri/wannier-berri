becb8f12f5f2ba4b58810e325f1eca2c00b42cb4
Stepan Tsirkin <stepan.tsirkin@ehu.eus>
2024-07-30 02:43:06 +0200

    added workflow

---
+++ wannierberri/workflow/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module is intended to automatize the workflow of runs 
+of qunatum espresso wannier90 and wannier-berri. Very experimental, 
+use on your own risk, mnodify if needed
+"""
+
+from .workflow import WorkflowQE, Executables
+
+
