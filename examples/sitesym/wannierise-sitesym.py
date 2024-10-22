73bc35ad2664af0c730941d069e06fb5e048bf3a
Stepan Tsirkin <stepan.tsirkin@ehu.eus>
2024-10-22 14:43:02 +0200

    example for wannierise diamond

--- examples/sitesym/wannierise-sitesym.py
+++ examples/sitesym/wannierise-sitesym.py
@@ -1,73 +1,64 @@
-import subprocess
-from matplotlib import pyplot as plt
+# This tutorial shows how to generate a DMN file inside Wanier Berri code (without pw2wannier90)
+# and then use it to generate a Wannier90 output.
+# It uses Irrep, and may be used with any DFT code that is supported by Irrep (QE, VASP, AINIT, ...)
+
+import os, shutil
 import wannierberri as wberri
-import shutil, os
-for ext in  ["mmn","amn","dmn","eig","win"]:
-    shutil.copy(os.path.join("../../tests/data/diamond","diamond." + ext),
-                 "diamond." + ext) 
 
-systems = {}
+data_dir = "../../tests/data/diamond"
 
-# Just fot Reference : run the Wannier90 with sitesym, but instead of frozen window use outer window
-# to exclude valence bands
-# os.system("wannier90.x diamond")
-subprocess.run(["wannier90.x", "diamond"])
-#record the system from the Wanier90 output ('diamond.chk' file)
-systems["w90"] = wberri.system.System_w90(seedname='diamond')
+for ext in ["mmn","amn","dmn","eig","win"]:
+    shutil.copy(os.path.join(data_dir, "diamond." + ext),
+                 os.path.join("./", "diamond." + ext))
+    
+
+
+# if the dmn file needs to be geneated, (if False - will be read from the file)
+generate_dmn = True
 
 # Read the data from the Wanier90 inputs 
 w90data = wberri.w90files.Wannier90data(seedname='diamond')
 
+if generate_dmn:
+    from irrep.bandstructure import BandStructure
+    bandstructure = BandStructure(code='espresso', 
+                                prefix=os.path.join(data_dir, "di"),
+                                Ecut=100,
+                                normalize=False, include_TR=False)
+    w90data.set_d_band(bandstructure)
+    pos = [[0,0,0],[0,0,1/2],[0,1/2,0],[1/2,0,0]]
+    w90data.set_D_wann_from_projections(projections=[(pos, 's') ])
+
+amn = w90data.amn
+dmn = w90data.dmn
+print (amn.data.shape)
+print ("amn_symmetry", dmn.check_amn(w90data.amn.data, warning_precision=1e-4))
+
 #Now disentangle with sitesym and frozen window (the part that is not implemented in Wanier90)
 w90data.wannierise(
-                froz_min=-8,
-                froz_max=17,
-                num_iter=000,
+                froz_min=0,
+                froz_max=4,
+                num_iter=1000,
                 conv_tol=1e-10,
                 mix_ratio_z=1.0,
                 mix_ratio_u=1.0,
                 print_progress_every=20,
-                sitesym=False,
-                localise=False,
+                sitesym=True,
+                localise=True,
                 )
 
-systems["wberri"] = wberri.system.System_w90(w90data=w90data)
+system = wberri.system.System_w90(w90data=w90data)
 
-# If needed - perform maximal localization using Wannier90
 
-# first generate the reduced files - where num_bands is reduced to num_wann,
-# by taking the optimized subspace
-w90data_reduced = w90data.get_disentangled(files = ["eig","mmn","amn","dmn"])
-w90data_reduced.write("diamond_disentangled", files = ["eig","mmn","amn","dmn"])
-
-# Now write the diamond_disentangled.win file
-# first take the existing file
-win_file = wberri.w90files.WIN(seedname='diamond')
-# and modify some parameters
-win_file["num_bands"] = win_file["num_wann"]
-win_file["dis_num_iter"] = 0
-win_file["num_iter"] = 1000
-del win_file["dis_froz_win"]
-del win_file["dis_froz_max"] 
-win_file["site_symmetry"] =True
-win_file.write("diamond_disentangled")
-subprocess.run(["wannier90.x", "diamond_disentangled"])
-systems["mlwf"] = wberri.system.System_w90(seedname="diamond_disentangled")
-
-# Now calculate bandstructure for each of the systems
-# for creating a path any of the systems will do the job
-system0 = list(systems.values())[0]
-path = wberri.Path(system0, k_nodes=[[0,0,0],[1/2,0,0]], labels=['G','L'], length=100)
+# Now calculate bandstructure 
+path = wberri.Path(system, k_nodes=[[0,0,0],[1/2,0,0],[1,0,0]], labels=['G','L','G'], length=100)
 tabulator = wberri.calculators.TabulatorAll(tabulators = {}, mode='path')
 calculators = {'tabulate': tabulator}
 
-linecolors = ['black', 'red', 'blue', 'green', 'orange', 'purple', 'brown', 'pink', 'gray', 'cyan']
-linestyles = ['-', '--', ':', '-.', ]*4
-for key,sys in systems.items():
-    result = wberri.run(sys, grid=path, calculators=calculators)
-    result.results['tabulate'].plot_path_fat(path, close_fig=False, show_fig=False, 
-                                             linecolor=linecolors.pop(0), label=key,
-                                             kwargs_line={"ls":linestyles.pop(0)})
-plt.savefig("diamond-bands.png")
+result = wberri.run(system, grid=path, calculators=calculators)
+result.results['tabulate'].plot_path_fat(path,
+                                         save_file='diamond-bands.pdf',
+                                         close_fig=True,
+                                         show_fig=False,
+                                         )
 
-# One can see that results do not differ much. Also, the maximal localization does not have much effect.
